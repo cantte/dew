0643f7b40f8154044f4a01bb33a9a47a80a55a20
cantte <cantte098@gmail.com>
2023-12-13 12:23:37 -0500

    feat: add product list view
    Part of DEW-2

---
+++ src/app/(dashboard)/dashboard/products/columns.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { type ColumnDef } from "@tanstack/react-table";
+import { type RouterOutputs } from "~/trpc/shared";
+import Link from "next/link";
+import { Button } from "~/components/ui/button";
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+
+export type Product = RouterOutputs["product"]["list"][number];
+
+export const columns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Nombre",
+  },
+  {
+    accessorKey: "salePrice",
+    header: "Precio de venta",
+  },
+  {
+    accessorKey: "stock",
+    header: "Stock",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      return (
+        <div className="flex">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button asChild variant="secondary" size="icon">
+                  <Link href={`/products/edit/${row.original.id}`}>
+                    <Pencil2Icon />
+                  </Link>
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p>Editar</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      );
+    },
+  },
+];
