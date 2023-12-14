4974741b331feb68c9c05aa7612c6b5e04e809dd
cantte <cantte098@gmail.com>
2023-12-13 19:50:13 -0500

    feat: add footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { getServerAuthSession } from "~/server/auth";
 import NextLink from "next/link";
 import { Button } from "~/components/ui/button";
 import { Badge } from "~/components/ui/badge";
+import Footer from "~/components/footer";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -90,21 +91,7 @@ export default async function Home() {
         </span>
       </div>
 
-      <footer className="flex h-[80px] flex-col items-center justify-center text-center">
-        <span className="inline-flex items-center gap-2 text-sm font-normal text-muted-foreground">
-          {new Date().getFullYear()} © dew
-        </span>
-        <span className="inline-flex items-center gap-2 text-sm font-normal text-muted-foreground">
-          Creado con <span className="text-red-500">❤</span> por
-          <a
-            href="https://github.com/cantte"
-            target="_blank"
-            className="text-foreground transition-colors duration-200 hover:text-foreground/80"
-          >
-            cantte
-          </a>
-        </span>
-      </footer>
+      <Footer />
     </main>
   );
 }
