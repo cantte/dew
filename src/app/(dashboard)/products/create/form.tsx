e0d72b5c0714996bee33df9f40b84fa1013b997a
cantte <cantte098@gmail.com>
2023-12-13 20:42:16 -0500

    feat:(products): add quantity field

--- src/app/(dashboard)/products/create/form.tsx
+++ src/app/(dashboard)/products/create/form.tsx
@@ -138,6 +138,21 @@ const CreateProductForm = () => {
           )}
         />
 
+        <FormField
+          control={form.control}
+          name="quantity"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Existencia</FormLabel>
+              <FormControl>
+                <Input type="number" {...field} />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
         <Button type="submit" disabled={createProduct.isLoading}>
           {createProduct.isLoading && (
             <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
