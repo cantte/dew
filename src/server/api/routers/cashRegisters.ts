12b5dfc5adf6b38808e0f2dc1eba9053eec4af92
cantte <cantte098@gmail.com>
2024-02-20 23:02:23 -0500

    feat(cash-register): update amount on new transaction
    part of DEW-13

--- src/server/api/routers/cashRegisters.ts
+++ src/server/api/routers/cashRegisters.ts
@@ -1,4 +1,4 @@
-import { and, between, eq } from "drizzle-orm";
+import { and, between, desc, eq } from "drizzle-orm";
 import { v4 as uuid } from "uuid";
 import { z } from "zod";
 
@@ -34,10 +34,51 @@ export const cashRegistersRouter = createTRPCRouter({
     create: protectedProcedure
       .input(createCashRegisterTransactionInput)
       .mutation(async ({ ctx, input }) => {
-        await ctx.db.insert(cashRegisterTransactions).values({
-          ...input,
-          id: uuid(),
-          createdBy: ctx.session.user.id,
+        await ctx.db.transaction(async (tx) => {
+          const [cashRegister] = await tx
+            .select({
+              amount: cashRegisters.amount,
+            })
+            .from(cashRegisters)
+            .where(eq(cashRegisters.id, input.cashRegisterId));
+
+          if (cashRegister === undefined) {
+            try {
+              tx.rollback();
+            } catch (e) {
+              throw new Error("Cash register not found");
+            }
+          }
+
+          if (cashRegister === undefined) {
+            return;
+          }
+
+          if (input.type === "OUT" && cashRegister.amount < input.amount) {
+            try {
+              tx.rollback();
+            } catch (e) {
+              throw new Error("Insufficient cash register amount");
+            }
+          }
+
+          await tx.insert(cashRegisterTransactions).values({
+            ...input,
+            id: uuid(),
+            createdBy: ctx.session.user.id,
+          });
+
+          const newAmount =
+            input.type === "IN"
+              ? cashRegister.amount + input.amount
+              : cashRegister.amount - input.amount;
+
+          await tx
+            .update(cashRegisters)
+            .set({
+              amount: newAmount,
+            })
+            .where(eq(cashRegisters.id, input.cashRegisterId));
         });
       }),
     list: protectedProcedure
@@ -50,10 +91,14 @@ export const cashRegistersRouter = createTRPCRouter({
       )
       .query(async ({ ctx, input }) => {
         return ctx.db.query.cashRegisterTransactions.findMany({
+          with: {
+            user: true,
+          },
           where: and(
             eq(cashRegisterTransactions.cashRegisterId, input.cashRegisterId),
             between(cashRegisterTransactions.createdAt, input.from, input.to),
           ),
+          orderBy: [desc(cashRegisterTransactions.createdAt)],
         });
       }),
   }),
