677a48fa52d77c16f45144ec1bc313a974f9a6c9
cantte <cantte098@gmail.com>
2024-02-23 16:58:48 -0500

    feat: add dashboard animations

---
+++ src/app/(dashboard)/dashboard/template.tsx
@@ -0,0 +1,16 @@
+import { type ReactNode } from "react";
+import { MotionDiv } from "~/components/motion-div";
+
+type Props = {
+  children: ReactNode;
+};
+
+const RootTemplate = ({ children }: Props) => {
+  return (
+    <MotionDiv initial={{ opacity: 0, y: 40 }} animate={{ opacity: 1, y: 0 }}>
+      {children}
+    </MotionDiv>
+  );
+};
+
+export default RootTemplate;
