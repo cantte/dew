61258c91461c19fcda72e61101222059fda8c2b5
cantte <cantte098@gmail.com>
2024-02-23 12:43:51 -0500

    chore: remove unused type

--- src/components/signout-button.tsx
+++ src/components/signout-button.tsx
@@ -3,10 +3,6 @@
 import { signOut } from "next-auth/react";
 import { Button } from "~/components/ui/button";
 
-type Props = {
-  className?: string;
-};
-
 const SignOutButton = () => {
   return (
     <Button variant="ghost" size="sm" onClick={() => void signOut()}>
