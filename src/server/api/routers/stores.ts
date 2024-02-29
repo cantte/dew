b50c72ef7dcc0c66a1729c20e55bcf6219879e91
cantte <cantte098@gmail.com>
2024-02-29 11:55:53 -0500

    feat(stores): set user store id after register

--- src/server/api/routers/stores.ts
+++ src/server/api/routers/stores.ts
@@ -2,17 +2,30 @@ import { eq } from "drizzle-orm";
 import { v4 as uuid } from "uuid";
 import { createStoreInput } from "~/server/api/schemas/stores";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
-import { stores } from "~/server/db/schema";
+import { stores, userPreferences } from "~/server/db/schema";
 
 export const storesProcedure = createTRPCRouter({
   create: protectedProcedure
     .input(createStoreInput)
     .mutation(async ({ ctx, input }) => {
+      const storeId = uuid();
       await ctx.db.insert(stores).values({
         ...input,
-        id: uuid(),
+        id: storeId,
         createdBy: ctx.session.user.id,
       });
+
+      await ctx.db
+        .insert(userPreferences)
+        .values({
+          userId: ctx.session.user.id,
+          storeId: storeId,
+        })
+        .onDuplicateKeyUpdate({
+          set: {
+            storeId,
+          },
+        });
     }),
   find: protectedProcedure.query(async ({ ctx }) => {
     return ctx.db.query.stores.findFirst({
