cadd292cb6c7dad8e11700a7a728987726622bd0
cantte <cantte098@gmail.com>
2024-03-01 14:45:31 -0500

    feat: update mobile nav

--- src/app/(dashboard)/dashboard/dashboard.tsx
+++ src/app/(dashboard)/dashboard/dashboard.tsx
@@ -76,7 +76,7 @@ const Dashboard = ({
 
       <DateRangeFilter selected={date} onSelectRange={setDate} />
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-2">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Ingresos</CardTitle>
