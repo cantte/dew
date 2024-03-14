c68968022233ee853d9406415f6d8290da8920e1
cantte <cantte098@gmail.com>
2024-03-14 12:48:13 -0500

    feat: add loading indicator

---
+++ src/components/default-loading.tsx
@@ -0,0 +1,12 @@
+import { Skeleton } from "~/components/ui/skeleton";
+
+const DefaultLoading = () => {
+  return (
+    <div className="flex flex-col space-y-3">
+      <Skeleton className="h-4 w-1/2" />
+      <Skeleton className="h-4 w-1/3" />
+    </div>
+  );
+};
+
+export default DefaultLoading;
