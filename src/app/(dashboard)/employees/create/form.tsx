9bef41be547002b95ced38b775d8764b0abfa732
cantte <cantte098@gmail.com>
2024-04-01 12:26:26 -0500

    feat(employees): show alert when employee already exists
    completes DEW-28

--- src/app/(dashboard)/employees/create/form.tsx
+++ src/app/(dashboard)/employees/create/form.tsx
@@ -2,10 +2,21 @@
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { ReloadIcon } from "@radix-ui/react-icons";
+import { useDebounce } from "@uidotdev/usehooks";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { type z } from "zod";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "~/components/ui/alert-dialog";
 import { Button } from "~/components/ui/button";
 import {
   Form,
@@ -51,80 +62,117 @@ const CreateEmployeeForm = ({ storeId }: Props) => {
     createEmployee.mutate(data);
   };
 
+  const employeeId = useDebounce(form.watch("id"), 1000);
+  const { data: employee } = api.employee.find.useQuery(
+    { id: employeeId },
+    {
+      enabled: employeeId !== "",
+    },
+  );
+
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    if (employee) {
+      setIsOpen(true);
+    }
+  }, [employee]);
+
   return (
-    <Form {...form}>
-      <form
-        className="flex flex-col space-y-4"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        <FormField
-          control={form.control}
-          name="id"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Identificación</FormLabel>
-              <FormControl>
-                <Input autoFocus {...field} />
-              </FormControl>
+    <Fragment>
+      <AlertDialog open={isOpen} onOpenChange={setIsOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Empleado encontrado</AlertDialogTitle>
+            <AlertDialogDescription>
+              Ya existe un empleado con la identificación ingresada. ¿Desea
+              cargar los datos del empleado?
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+            <AlertDialogAction
+              autoFocus
+              onClick={() => form.reset(employee as FormValues)}
+            >
+              Cargar datos
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+      <Form {...form}>
+        <form
+          className="flex flex-col space-y-4"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="id"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Identificación</FormLabel>
+                <FormControl>
+                  <Input autoFocus {...field} />
+                </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre completo</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre completo</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" {...field} />
-              </FormControl>
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} />
+                </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <FormField
-          control={form.control}
-          name="phone"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Teléfono</FormLabel>
-              <FormControl>
-                <Input {...field} />
-              </FormControl>
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Teléfono</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
 
-        <Button type="submit" disabled={createEmployee.isLoading}>
-          {createEmployee.isLoading && (
-            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-          )}
-          Crear
-        </Button>
-      </form>
-    </Form>
+          <Button type="submit" disabled={createEmployee.isLoading}>
+            {createEmployee.isLoading && (
+              <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+            )}
+            Crear
+          </Button>
+        </form>
+      </Form>
+    </Fragment>
   );
 };
 
