515015bc0b554322025acea36b1e768bdf4a830f
Carlos Pavajeau <cantte098@gmail.com>
2024-05-14 18:20:44 -0500

    chore: update prettier.config.js to ignore package.json and pnpm-lock.yaml

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,7 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  ignorePatterns: ["package.json", "pnpm-lock.yaml"],
 };
 
 export default config;
