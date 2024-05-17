44e1c600cfc5124b5ff68f3237b8393c8725026d
Carlos Pavajeau <cantte098@gmail.com>
2024-05-17 17:08:08 -0500

    refactor: add createdAt field to store selection in storesProcedure

--- src/server/api/routers/stores.ts
+++ src/server/api/routers/stores.ts
@@ -69,6 +69,7 @@ export const storesProcedure = createTRPCRouter({
       .select({
         id: stores.id,
         name: stores.name,
+        createdAt: stores.createdAt,
       })
       .from(employeeStore)
       .innerJoin(stores, eq(employeeStore.storeId, stores.id))
