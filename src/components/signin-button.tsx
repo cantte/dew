74612bdbf8a9e1506aaafbb70ed328fa30ef73c0
Carlos Pavajeau <cantte098@gmail.com>
2024-05-17 14:03:20 -0500

    refactor: update callback URL in signin-button component
    Update the callback URL in the signin-button component to "/dashboard" instead of the previous default value. This change ensures that users are redirected to the dashboard after signing in.

--- src/components/signin-button.tsx
+++ src/components/signin-button.tsx
@@ -12,7 +12,7 @@ const SignInButton = () => {
     <Button
       onClick={() =>
         void signIn("google", {
-          callbackUrl: callbackUrl ?? "/",
+          callbackUrl: callbackUrl ?? "/dashboard",
         })
       }
     >
