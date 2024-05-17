bef75033370a176f79eeba0bc2cef2dbc132a5bf
Carlos Pavajeau <cantte098@gmail.com>
2024-05-17 11:41:24 -0500

    feat: add employee store invitation email template

---
+++ src/emails/employee-store-invitation.tsx
@@ -0,0 +1,65 @@
+import {
+    Body,
+    Button,
+    Container,
+    Head,
+    Hr,
+    Html,
+    Preview,
+    Tailwind,
+    Text,
+} from "@react-email/components";
+
+type Props = {
+  employeeName: string;
+  storeName: string;
+  url: string;
+};
+
+const EmployeeStoreInvitationEmail = ({
+  employeeName,
+  storeName,
+  url,
+}: Props) => {
+  return (
+    <Html>
+      <Head />
+      <Preview>Invitación a la tienda</Preview>
+      <Tailwind>
+        <Body className="mx-auto my-auto px-2 font-sans">
+          <Container className="mx-auto my-[40px] max-w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
+            <Text className="scroll-m-20 text-xl font-semibold tracking-tight">
+              Hola {employeeName}, has sido invitado a la tienda {storeName}.
+            </Text>
+
+            <Text>
+              Puedes acceder a la tienda dando clic en el siguiente enlace:
+            </Text>
+
+            <Container className="flex justify-center">
+              <Button
+                className="rounded-md bg-black px-4 py-2 text-center text-sm font-medium text-white"
+                href={url}
+              >
+                Acceder a la tienda
+              </Button>
+            </Container>
+
+            <Text className="text-xs text-neutral-500">
+              Este enlace es personal e intransferible y solo puede ser usado
+              por ti una vez.
+            </Text>
+
+            <Hr />
+
+            <Text className="text-xs text-neutral-500">
+              © {new Date().getFullYear()} Dew. Todos los derechos reservados.
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+};
+
+export default EmployeeStoreInvitationEmail;
