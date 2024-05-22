097259fa87f3e7e540b13d0dfdd418c7d737b984
Carlos Pavajeau <cantte098@gmail.com>
2024-05-21 22:10:55 -0500

    refactor: improve sale detail ui

--- src/app/(dashboard)/sales/c/[code]/page.tsx
+++ src/app/(dashboard)/sales/c/[code]/page.tsx
@@ -1,7 +1,12 @@
 import { notFound } from "next/navigation";
-import React from "react";
 import SaleDetail from "~/components/sale-detail";
 import { Badge } from "~/components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
 import { api } from "~/trpc/server";
 
 type Props = {
@@ -20,10 +25,22 @@ const CustomerSaleDetailPage = async ({ params }: Props) => {
   return (
     <div className="space-y-4">
       <div className="flex items-center gap-2">
-        <h1 className="text-2xl font-bold">Detalle de venta</h1>
-        <Badge>
-          {sale.paymentMethod === "cash" ? "Efectivo" : "No registrado"}
-        </Badge>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Badge variant="outline">
+                {Intl.DateTimeFormat("es-CO", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                }).format(new Date(sale.createdAt))}
+              </Badge>
+            </TooltipTrigger>
+            <TooltipContent>Fecha de creación de la venta</TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </div>
 
       <SaleDetail sale={sale} />
