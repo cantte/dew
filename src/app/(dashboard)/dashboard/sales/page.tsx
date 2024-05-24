2ea8d29f818a541c946b2490199aa3b1632ec763
Carlos Pavajeau <cantte098@gmail.com>
2024-05-24 11:23:01 -0500

    refactor: add SalesOverview component to display sales overview in sales page

--- src/app/(dashboard)/dashboard/sales/page.tsx
+++ src/app/(dashboard)/dashboard/sales/page.tsx
@@ -1,5 +1,7 @@
+import { endOfDay, startOfDay } from "date-fns";
 import { columns } from "~/app/(dashboard)/dashboard/sales/columns";
 import SalesDataTable from "~/app/(dashboard)/dashboard/sales/data-table";
+import SalesOverview from "~/app/(dashboard)/dashboard/sales/overview";
 import NotFoundStoreAlert from "~/components/stores/not-found.alert";
 import { api } from "~/trpc/server";
 
@@ -14,15 +16,18 @@ const SalesPage = async () => {
     storeId: store.id,
   });
 
-  return (
-    <div>
-      <div className="flex justify-between">
-        <span className="font-semibold tracking-tight">Ventas</span>
-      </div>
+  const from = startOfDay(new Date());
+  const to = endOfDay(new Date());
+  const overview = await api.sale.overview({
+    storeId: store.id,
+    from,
+    to,
+  });
 
-      <div className="mt-4">
-        <SalesDataTable columns={columns} data={sales} storeId={store.id} />
-      </div>
+  return (
+    <div className="space-y-4">
+      <SalesOverview overview={overview} />
+      <SalesDataTable columns={columns} data={sales} storeId={store.id} />
     </div>
   );
 };
