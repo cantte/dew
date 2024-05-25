fddbef75c8cf2a53415c163504e5b367d8db0557
Carlos Pavajeau <cantte098@gmail.com>
2024-05-25 16:46:46 -0500

    chore: update link in ProductsDataTableToolbar to create products page

--- src/app/(dashboard)/dashboard/products/data-table-toolbar.tsx
+++ src/app/(dashboard)/dashboard/products/data-table-toolbar.tsx
@@ -41,7 +41,7 @@ const ProductsDataTableToolbar = <TData,>({
 
       <div className="flex space-x-2">
         <Button asChild size="sm" className="h-7 gap-1">
-          <Link href="/sales/create">
+          <Link href="/products/create">
             <PlusCircle className="h-3.5 w-3.5" />
             <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
               Crear producto
