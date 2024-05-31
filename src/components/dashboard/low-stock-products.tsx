eea85a7e8a06eb8a76881e0b2144a85c83b3b947
Carlos Pavajeau <cantte098@gmail.com>
2024-05-31 16:21:21 -0500

    feat: Add new components for dashboard
    - Added ValueDateTooltip component to display value and date information
    - Added LowStockProducts component to display low stock products
    - Added MostSoldProducts component to display most sold products
    
    These components will enhance the dashboard functionality by providing valuable information to the users.

---
+++ src/components/dashboard/low-stock-products.tsx
@@ -0,0 +1,74 @@
+import {
+    type ColumnDef,
+    getCoreRowModel,
+    getFacetedRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import DataTable from "~/components/data-table";
+import type { RouterOutputs } from "~/trpc/shared";
+
+type Props = {
+  lowStockProducts: RouterOutputs["inventory"]["lowStock"];
+};
+
+type LowStockProduct = RouterOutputs["inventory"]["lowStock"][number];
+
+const columns: ColumnDef<LowStockProduct>[] = [
+  {
+    header: "Producto",
+    accessorKey: "name",
+  },
+  {
+    header: "Cantidad",
+    accessorKey: "quantity",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {Intl.NumberFormat("es-CO").format(+(row.original.quantity ?? 0))}
+        </span>
+      );
+    },
+  },
+  {
+    header: "Stock",
+    accessorKey: "stock",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {Intl.NumberFormat("es-CO").format(+(row.original.stock ?? 0))}
+        </span>
+      );
+    },
+  },
+];
+
+const LowStockProducts = ({ lowStockProducts }: Props) => {
+  const table = useReactTable<LowStockProduct>({
+    data: lowStockProducts,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+  });
+
+  return (
+    <>
+      {lowStockProducts.length > 0 && (
+        <div className="flex flex-col space-y-2">
+          <span className="font-semibold tracking-tight">
+            Productos bajos en stock
+          </span>
+
+          <DataTable table={table} />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default LowStockProducts;
---
+++ src/components/dashboard/most-sold-products.tsx
@@ -0,0 +1,95 @@
+import {
+    type ColumnDef,
+    getCoreRowModel,
+    getFacetedRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
+} from "@tanstack/react-table";
+import DataTable from "~/components/data-table";
+import type { RouterOutputs } from "~/trpc/shared";
+
+type Props = {
+  mostSoldProducts: RouterOutputs["sale"]["mostSoldProducts"];
+};
+
+type MostSoldProduct = RouterOutputs["sale"]["mostSoldProducts"][number];
+
+const columns: ColumnDef<MostSoldProduct>[] = [
+  {
+    header: "Producto",
+    accessorKey: "name",
+  },
+  {
+    header: "Volumen de ventas",
+    accessorKey: "quantity",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {Intl.NumberFormat("es-CO").format(+(row.original.quantity ?? 0))}
+        </span>
+      );
+    },
+  },
+  {
+    header: "Ingresos",
+    accessorKey: "amount",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {Intl.NumberFormat("es-CO", {
+            style: "currency",
+            currency: "COP",
+          }).format(+(row.original.amount ?? 0))}
+        </span>
+      );
+    },
+  },
+  {
+    header: "Ganancias",
+    accessorKey: "profit",
+    cell: ({ row }) => {
+      return (
+        <span>
+          {Intl.NumberFormat("es-CO", {
+            style: "currency",
+            currency: "COP",
+          }).format(+(row.original.profit ?? 0))}
+        </span>
+      );
+    },
+  },
+];
+
+const MostSoldProducts = ({ mostSoldProducts }: Props) => {
+  const table = useReactTable<MostSoldProduct>({
+    data: mostSoldProducts,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    getFacetedRowModel: getFacetedRowModel(),
+  });
+
+  return (
+    <div className="flex flex-col space-y-2">
+      <span className="font-semibold tracking-tight">
+        Productos más vendidos
+      </span>
+      <div>
+        {mostSoldProducts.length > 0 ? (
+          <DataTable table={table} />
+        ) : (
+          <div className="flex flex-col items-center justify-center">
+            <h3 className="text-2xl font-semibold">No hay productos</h3>
+            <p className="text-gray-500">No se han vendido productos aún</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default MostSoldProducts;
