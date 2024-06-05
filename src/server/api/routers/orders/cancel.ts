11a69e243ebb38fda1725e7f8ed918c70aa3e4fe
Carlos Pavajeau <cantte098@gmail.com>
2024-06-05 15:35:52 -0500

    feat(orders): add cancelOrder function and cancel route

---
+++ src/server/api/routers/orders/cancel.ts
@@ -0,0 +1,29 @@
+import { eq } from "drizzle-orm";
+import { v4 as uuid } from "uuid";
+import type { TypeOf } from "zod";
+import type { TRPCAuthedContext } from "~/server/api/procedures/authed";
+import type { byOrderIdInput } from "~/server/api/schemas/orders";
+import { orderHistory, orders } from "~/server/db/schema";
+
+type Options = {
+  ctx: TRPCAuthedContext;
+  input: TypeOf<typeof byOrderIdInput>;
+};
+
+const cancelOrder = async ({ ctx, input }: Options) => {
+  await ctx.db.transaction(async (tx) => {
+    await tx
+      .update(orders)
+      .set({ state: "cancelled" })
+      .where(eq(orders.id, input.id));
+
+    await tx.insert(orderHistory).values({
+      id: uuid(),
+      orderId: input.id,
+      state: "cancelled",
+      createdBy: ctx.session.user.id,
+    });
+  });
+};
+
+export default cancelOrder;
--- src/server/api/routers/orders/index.ts
+++ src/server/api/routers/orders/index.ts
@@ -1,8 +1,9 @@
 import authedProcedure from "~/server/api/procedures/authed";
+import cancelOrder from "~/server/api/routers/orders/cancel";
 import createOrder from "~/server/api/routers/orders/create";
 import listOrders from "~/server/api/routers/orders/list";
 import { byStoreInput } from "~/server/api/schemas/common";
-import { createOrderInput } from "~/server/api/schemas/orders";
+import { byOrderIdInput, createOrderInput } from "~/server/api/schemas/orders";
 import { router } from "~/server/api/trpc";
 
 const ordersRouter = router({
@@ -14,6 +15,11 @@ const ordersRouter = router({
   list: authedProcedure.input(byStoreInput).query(async ({ ctx, input }) => {
     return await listOrders({ ctx, input });
   }),
+  cancel: authedProcedure
+    .input(byOrderIdInput)
+    .mutation(async ({ ctx, input }) => {
+      await cancelOrder({ ctx, input });
+    }),
 });
 
 export default ordersRouter;
