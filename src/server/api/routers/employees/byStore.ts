e114fda6068ca919f166fcca135dfe58f1e82f8b
Carlos Pavajeau <cantte098@gmail.com>
2024-06-05 22:11:33 -0500

    fix(employees): correctly determine the value of isOwner

--- src/server/api/routers/employees/byStore.ts
+++ src/server/api/routers/employees/byStore.ts
@@ -2,7 +2,7 @@ import { desc, eq } from "drizzle-orm";
 import type { TypeOf } from "zod";
 import type { TRPCAuthedContext } from "~/server/api/procedures/authed";
 import type { byStoreInput } from "~/server/api/schemas/common";
-import { employeeStore, employees } from "~/server/db/schema";
+import { employeeStore, employees, stores } from "~/server/db/schema";
 
 type Options = {
   ctx: TRPCAuthedContext;
@@ -16,13 +16,14 @@ const findEmployeesByStore = async ({ ctx, input }: Options) => {
       name: employees.name,
       email: employees.email,
       phone: employees.phone,
-      isOwner: eq(employees.id, employeeStore.employeeId).mapWith(Boolean),
+      isOwner: eq(employees.userId, stores.createdBy).mapWith(Boolean),
       isCurrentEmployee: eq(employees.userId, ctx.session.user.id).mapWith(
         Boolean,
       ),
     })
     .from(employees)
     .innerJoin(employeeStore, eq(employees.id, employeeStore.employeeId))
+    .innerJoin(stores, eq(employeeStore.storeId, stores.id))
     .where(eq(employeeStore.storeId, input.storeId))
     .orderBy(desc(employees.createdAt));
 };
