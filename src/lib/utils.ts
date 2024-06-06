5fe7d4f7aacb4d8573f206c9c2ae798b121cdbcc
Carlos Pavajeau <cantte098@gmail.com>
2024-06-06 15:01:19 -0500

    feat(utils): add getOrderStatusLabel function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,11 @@
-import { type ClassValue, clsx } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { orderStatus } from "~/constants";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const getOrderStatusLabel = (status: string) => {
+  return orderStatus.find((s) => s.id === status)?.label;
+};
