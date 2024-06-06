03c7d1b8a60a77909e7d773b4c117f29319d580b
Carlos Pavajeau <cantte098@gmail.com>
2024-06-06 15:04:48 -0500

    chore: format code

--- src/components/orders/history.dialog.tsx
+++ src/components/orders/history.dialog.tsx
@@ -5,11 +5,11 @@ import { Suspense } from "react";
 import OrderHistory from "~/components/orders/history";
 import { Button } from "~/components/ui/button";
 import {
-    Dialog,
-    DialogContent,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "~/components/ui/dialog";
 import type { RouterOutputs } from "~/trpc/shared";
 
