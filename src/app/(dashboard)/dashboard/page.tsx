1181940b8c5b299b5af103f9621dd904bb48fa1d
Carlos Pavajeau <cantte098@gmail.com>
2024-06-12 12:52:22 -0500

    feat: revamp dashboard page
    use server components with suspence to improve dashboard loading

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,12 @@
-import { endOfMonth, startOfMonth } from "date-fns";
-import Dashboard from "~/app/(dashboard)/dashboard/dashboard";
+import { Suspense } from "react";
+import SalesOverview from "~/app/(dashboard)/dashboard/sales/overview";
+import LowStockProducts from "~/components/dashboard/low-stock-products";
+import MostSoldProducts from "~/components/dashboard/most-sold-products";
+import OrdersReport from "~/components/dashboard/orders-report";
+import SalesReport from "~/components/dashboard/sales-report";
 import NotFoundStoreAlert from "~/components/stores/not-found.alert";
+import { Skeleton } from "~/components/ui/skeleton";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
 import { api } from "~/trpc/server";
 
 const DashboardPage = async () => {
@@ -9,42 +15,57 @@ const DashboardPage = async () => {
     return <NotFoundStoreAlert />;
   }
 
-  const from = startOfMonth(new Date());
-  const to = endOfMonth(new Date());
-
-  const saleOverview = await api.sale.overview({
-    from,
-    to,
-    storeId: store.id,
-  });
-
-  const salesReport = await api.sale.report({
-    from,
-    to,
-    storeId: store.id,
-  });
-
-  const ordersReport = await api.order.report({
-    from,
-    to,
-    storeId: store.id,
-  });
-
-  const mostSoldProducts = await api.sale.mostSoldProducts();
-  const lowStockProducts = await api.inventory.lowStock({
-    storeId: store.id,
-  });
-
   return (
-    <main>
-      <Dashboard
-        saleOverview={saleOverview}
-        salesReport={salesReport}
-        ordersReport={ordersReport}
-        mostSoldProducts={mostSoldProducts}
-        lowStockProducts={lowStockProducts}
-      />
+    <main className="space-y-4">
+      <Tabs defaultValue="sales">
+        <TabsList>
+          <TabsTrigger value="sales">Resumen de ventas</TabsTrigger>
+          <TabsTrigger value="orders">Resumen de ordenes</TabsTrigger>
+        </TabsList>
+        <TabsContent value="sales">
+          <div className="space-y-4">
+            <Suspense
+              fallback={<Skeleton className="h-5 w-full max-w-[450px]" />}
+            >
+              <SalesOverview />
+            </Suspense>
+
+            <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
+              <Suspense fallback={<SalesReportFallback />}>
+                <SalesReport />
+              </Suspense>
+            </div>
+          </div>
+        </TabsContent>
+
+        <TabsContent value="orders">
+          <div className="space-y-4">
+            <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
+              <Suspense fallback={<SalesReportFallback />}>
+                <OrdersReport />
+              </Suspense>
+            </div>
+          </div>
+        </TabsContent>
+      </Tabs>
+
+      <Suspense fallback={<Skeleton className="h-10 w-full" />}>
+        <MostSoldProducts />
+      </Suspense>
+
+      <Suspense fallback={<Skeleton className="h-10 w-full" />}>
+        <LowStockProducts />
+      </Suspense>
     </main>
+  );
+};
+
+const SalesReportFallback = () => {
+  return (
+    <>
+      <Skeleton className="h-[200px] w-full" />
+      <Skeleton className="h-[200px] w-full" />
+    </>
   );
 };
 
---
+++ src/components/dashboard/sales-report.tsx
@@ -0,0 +1,40 @@
+import { endOfMonth, startOfMonth } from "date-fns";
+import TotalProfit from "~/components/dashboard/total-profit";
+import TotalRevenue from "~/components/dashboard/total-revenue";
+
+import { api } from "~/trpc/server";
+
+const SalesReport = async () => {
+  const store = await api.store.findCurrent();
+  if (!store) {
+    return null;
+  }
+
+  const today = new Date();
+  const from = startOfMonth(today);
+  const to = endOfMonth(today);
+
+  const report = await api.sale.report({
+    from,
+    to,
+    storeId: store.id,
+  });
+
+  return (
+    <>
+      <TotalRevenue
+        revenue={report.totalAmount}
+        revenueImprovement={report.amountImprovement}
+        revenueData={report.totalAmountPerDay}
+      />
+
+      <TotalProfit
+        profit={report.totalProfit}
+        profitImprovement={report.profitImprovement}
+        profitData={report.totalProfitPerDay}
+      />
+    </>
+  );
+};
+
+export default SalesReport;
