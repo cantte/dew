5f02da7416a18536931af3f568e88b9526e666ff
Carlos Pavajeau <cantte098@gmail.com>
2024-06-17 09:57:14 -0500

    feat(orders): add RBAC permission check when creating an order

--- src/app/(dashboard)/orders/create/page.tsx
+++ src/app/(dashboard)/orders/create/page.tsx
@@ -1,12 +1,22 @@
 import CreateOrderForm from "~/app/(dashboard)/orders/create/form";
 import BackButton from "~/components/back-button";
+import NotEnoughPermissions from "~/components/not-enough-permissions";
 import NotFoundStoreAlert from "~/components/stores/not-found.alert";
 import { api } from "~/trpc/server";
 
 const CreateOrderPage = async () => {
   const store = await api.store.findCurrent();
+
   if (!store) {
     return <NotFoundStoreAlert />;
+  }
+
+  const hasPermissions = await api.rbac.checkPermissions({
+    permissions: ["order:create", "product:view"],
+  });
+
+  if (!hasPermissions) {
+    return <NotEnoughPermissions />;
   }
 
   const suggestions = await api.product.suggestions({
