6e75609bb81b584d9a46456d4790a9ddd28a580f
Carlos Pavajeau <cantte098@gmail.com>
2024-06-16 22:58:07 -0500

    feat: Add RBAC router for checking permissions

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,6 +4,7 @@ import employeesRouter from "~/server/api/routers/employees";
 import inventoryRouter from "~/server/api/routers/inventory";
 import ordersRouter from "~/server/api/routers/orders";
 import productsRouter from "~/server/api/routers/products";
+import rbacRouter from "~/server/api/routers/rbac";
 import salesRouter from "~/server/api/routers/sales";
 import storesProcedure from "~/server/api/routers/stores";
 import userPreferencesRouter from "~/server/api/routers/userPreferences";
@@ -19,6 +20,7 @@ export const appRouter = router({
   cashRegister: cashRegistersRouter,
   userPreference: userPreferencesRouter,
   employee: employeesRouter,
+  rbac: rbacRouter,
 });
 
 export const createCaller = createCallerFactory(appRouter);
