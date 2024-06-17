dfc84f6ecef3273ceb76fd521cc0ce5518249ba1
Carlos Pavajeau <cantte098@gmail.com>
2024-06-17 10:07:50 -0500

    feat: Add RBAC permission check for creating a store

--- src/app/(dashboard)/stores/create/page.tsx
+++ src/app/(dashboard)/stores/create/page.tsx
@@ -1,12 +1,22 @@
 import { redirect } from "next/navigation";
 import CreateStoreForm from "~/app/(dashboard)/stores/create/form";
+import NotEnoughPermissions from "~/components/not-enough-permissions";
 import { getServerAuthSession } from "~/server/auth";
+import { api } from "~/trpc/server";
 
 const CreateStorePage = async () => {
   const session = await getServerAuthSession();
 
   if (!session) {
     return redirect("/api/auth/signin");
+  }
+
+  const hasPermissions = await api.rbac.checkPermissions({
+    permissions: ["store:create"],
+  });
+
+  if (!hasPermissions) {
+    return <NotEnoughPermissions />;
   }
 
   return (
