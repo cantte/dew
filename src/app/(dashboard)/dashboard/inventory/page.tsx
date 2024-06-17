ae30948b180b1bad7517da877a8f9a38ad0ddc3c
Carlos Pavajeau <cantte098@gmail.com>
2024-06-17 10:13:49 -0500

    chore: Add RBAC permission check for inventory page

--- src/app/(dashboard)/dashboard/inventory/page.tsx
+++ src/app/(dashboard)/dashboard/inventory/page.tsx
@@ -1,5 +1,6 @@
 import { columns } from "~/app/(dashboard)/dashboard/inventory/columns";
 import InventoryDataTable from "~/app/(dashboard)/dashboard/inventory/data-table";
+import NotEnoughPermissions from "~/components/not-enough-permissions";
 import NotFoundStoreAlert from "~/components/stores/not-found.alert";
 import { api } from "~/trpc/server";
 
@@ -8,6 +9,14 @@ const InventoryPage = async () => {
 
   if (!store) {
     return <NotFoundStoreAlert />;
+  }
+
+  const hasPermissions = await api.rbac.checkPermissions({
+    permissions: ["inventory:create"],
+  });
+
+  if (!hasPermissions) {
+    return <NotEnoughPermissions />;
   }
 
   const productInventory = await api.inventory.list({
