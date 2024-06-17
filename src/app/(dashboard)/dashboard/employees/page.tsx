acdd8a96b1dde2441f9d082e94d97bf93bd00526
Carlos Pavajeau <cantte098@gmail.com>
2024-06-17 11:29:41 -0500

    feat(employees): Add conditional rendering for create employee button

--- src/app/(dashboard)/dashboard/employees/page.tsx
+++ src/app/(dashboard)/dashboard/employees/page.tsx
@@ -26,19 +26,25 @@ const EmployeesPage = async () => {
     storeId: store.id,
   });
 
+  const canCreateEmployee = await api.rbac.checkPermissions({
+    permissions: ["employee:create"],
+  });
+
   return (
     <div className="space-y-2">
       <div className="flex justify-between">
         <span></span>
 
-        <Button asChild size="sm" className="h-7 gap-1">
-          <Link href="/employees/create">
-            <PlusCircle className="h-3.5 w-3.5" />
-            <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
-              Crear empleado
-            </span>
-          </Link>
-        </Button>
+        {canCreateEmployee && (
+          <Button asChild size="sm" className="h-7 gap-1">
+            <Link href="/employees/create">
+              <PlusCircle className="h-3.5 w-3.5" />
+              <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
+                Crear empleado
+              </span>
+            </Link>
+          </Button>
+        )}
       </div>
 
       <EmployeeDataTable
