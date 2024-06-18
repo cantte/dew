cc384bc0cc10429df48e30a9045edb9940d4d54a
Carlos Pavajeau <cantte098@gmail.com>
2024-06-18 15:40:59 -0500

    refactor: add date check for search product discount

--- src/server/api/routers/products/searchDiscount.ts
+++ src/server/api/routers/products/searchDiscount.ts
@@ -1,4 +1,4 @@
-import { desc, eq } from "drizzle-orm";
+import { and, desc, eq, gt, isNull, lt } from "drizzle-orm";
 import type { TypeOf } from "zod";
 import type { TRPCAuthedContext } from "~/server/api/procedures/authed";
 import type { byProductIdInput } from "~/server/api/schemas/products";
@@ -10,8 +10,15 @@ type Options = {
 };
 
 const searchProductDiscounts = async ({ ctx, input }: Options) => {
+  const today = new Date().toISOString().split("T")[0]!;
+
   return await ctx.db.query.productsDiscounts.findMany({
-    where: eq(productsDiscounts.productId, input.id),
+    where: and(
+      isNull(productsDiscounts.deletedAt),
+      eq(productsDiscounts.productId, input.id),
+      lt(productsDiscounts.startDate, today),
+      gt(productsDiscounts.endDate, today),
+    ),
     orderBy: [desc(productsDiscounts.startDate)],
   });
 };
