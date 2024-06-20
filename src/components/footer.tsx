c6b1a818ac6f6954640992b180d38207a48c8458
Carlos Pavajeau <cantte098@gmail.com>
2024-06-20 17:34:23 -0500

    refactor: Update footer component to use Next.js Link instead of anchor tag

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="flex h-[80px] flex-col items-center justify-center text-center">
@@ -6,13 +8,13 @@ const Footer = () => {
       </span>
       <span className="inline-flex items-center gap-2 text-sm font-normal text-muted-foreground">
         Creado con <span className="text-red-500">❤</span> por
-        <a
+        <Link
           href="https://github.com/cantte"
           target="_blank"
           className="text-foreground transition-colors duration-200 hover:text-foreground/80"
         >
           cantte
-        </a>
+        </Link>
       </span>
     </footer>
   );
