53140414ccc9d586f143f0bb91c92f877483cade
Carlos Pavajeau <cantte098@gmail.com>
2024-06-20 17:40:56 -0500

    refactor: Update Form component to use React Hook Form

--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,17 +1,17 @@
-import * as React from "react";
 import type * as LabelPrimitive from "@radix-ui/react-label";
 import { Slot } from "@radix-ui/react-slot";
+import * as React from "react";
 import {
   Controller,
+  FormProvider,
+  useFormContext,
   type ControllerProps,
   type FieldPath,
   type FieldValues,
-  FormProvider,
-  useFormContext,
 } from "react-hook-form";
 
-import { cn } from "~/lib/utils";
 import { Label } from "~/components/ui/label";
+import { cn } from "~/lib/utils";
 
 const Form = FormProvider;
 
@@ -32,8 +32,9 @@ const FormField = <
 >({
   ...props
 }: ControllerProps<TFieldValues, TName>) => {
+  const name = React.useMemo(() => props.name, [props.name]);
   return (
-    <FormFieldContext.Provider value={{ name: props.name }}>
+    <FormFieldContext.Provider value={{ name: name }}>
       <Controller {...props} />
     </FormFieldContext.Provider>
   );
@@ -166,12 +167,13 @@ const FormMessage = React.forwardRef<
 FormMessage.displayName = "FormMessage";
 
 export {
-  useFormField,
   Form,
-  FormItem,
-  FormLabel,
   FormControl,
   FormDescription,
-  FormMessage,
   FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField
 };
+
