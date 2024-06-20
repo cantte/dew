573e3dbc1d8c101f8727781790af51edf935bf89
Carlos Pavajeau <cantte098@gmail.com>
2024-06-20 14:34:39 -0500

    feat: Add initial OrderDetailPage component to display order details

---
+++ src/app/(dashboard)/orders/c/[id]/page.tsx
@@ -0,0 +1,15 @@
+type Props = {
+  params: {
+    code: string;
+  };
+};
+
+const OrderDetailPage = async ({ params }: Props) => {
+  return (
+    <div>
+      <h1>Order Detail</h1>
+    </div>
+  );
+};
+
+export default OrderDetailPage;
