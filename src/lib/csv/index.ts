d32247449438e916f41ac3ee33a418cf3c08ca99
Carlos Pavajeau <cantte098@gmail.com>
2024-06-24 12:17:43 -0500

    feat: add exportToCsv function for generating and downloading CSV files

---
+++ src/lib/csv/index.ts
@@ -0,0 +1,15 @@
+import { download, generateCsv, type mkConfig } from "export-to-csv";
+
+export type ExportableToCsv = {
+  [k: string]: string | number | boolean | null;
+  [k: number]: string | number | boolean | null;
+};
+
+type ExportConfig = ReturnType<typeof mkConfig>;
+type Data = ExportableToCsv[];
+
+export const exportToCsv = (config: ExportConfig, data: Data) => {
+  const csv = generateCsv(config)(data);
+
+  download(config)(csv);
+};
