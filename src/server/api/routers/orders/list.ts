7d5094c85ee46821bce526ddede087cde3204517
Carlos Pavajeau <cantte098@gmail.com>
2024-06-26 13:58:44 -0500

    chore(api): optimize listOrders query for improved performance

--- src/server/api/routers/orders/list.ts
+++ src/server/api/routers/orders/list.ts
@@ -1,8 +1,8 @@
-import { desc, eq } from 'drizzle-orm'
+import { desc, eq, sql } from 'drizzle-orm'
 import type { TypeOf } from 'zod'
 import type { TRPCAuthedContext } from '~/server/api/procedures/authed'
 import type { byStoreInput } from '~/server/api/schemas/common'
-import { orders } from '~/server/db/schema'
+import { customers, orders } from '~/server/db/schema'
 
 type Options = {
   ctx: TRPCAuthedContext
@@ -10,18 +10,21 @@ type Options = {
 }
 
 const listOrders = async ({ ctx, input }: Options) => {
-  return await ctx.db.query.orders.findMany({
-    with: {
-      customer: {
-        columns: {
-          id: true,
-          name: true,
-        },
-      },
-    },
-    orderBy: [desc(orders.createdAt)],
-    where: eq(orders.storeId, input.storeId),
-  })
+  return await ctx.db
+    .select({
+      id: orders.id,
+      customer: sql<string>`CONCAT(${customers.name}, ' (', ${customers.id}, ')')`,
+      status: orders.status,
+      amount: orders.amount,
+      paymentMethod: orders.paymentMethod,
+      address: orders.address,
+      phone: orders.phone,
+      createdAt: orders.createdAt,
+    })
+    .from(orders)
+    .innerJoin(customers, eq(customers.id, orders.customerId))
+    .where(eq(orders.storeId, input.storeId))
+    .orderBy(desc(orders.createdAt))
 }
 
 export default listOrders
