46562783f3dea7102f92237de46e46a325c83f26
Carlos Pavajeau <cantte098@gmail.com>
2024-06-28 11:57:34 -0500

    chore: Update menu icons in menu.sql

--- src/server/db/seed/menu.sql
+++ src/server/db/seed/menu.sql
@@ -1,9 +1,9 @@
 INSERT INTO "menu_item" (id, title, href, icon, priority, parent_id) VALUES 
-    ('d1b228de-b103-4793-a86b-82b864f09623', 'Panel de control', '/dashboard', 'dashboard', 10, NULL),
-    ('6c8181e3-1dcb-4392-a4e7-275c3d2e6ac8', 'Productos', '/dashboard/products', 'basket', 9, NULL),
+    ('d1b228de-b103-4793-a86b-82b864f09623', 'Panel de control', '/dashboard', 'home', 10, NULL),
+    ('6c8181e3-1dcb-4392-a4e7-275c3d2e6ac8', 'Productos', '/dashboard/products', 'shopping-basket', 9, NULL),
     ('a3c5b460-636f-4bdb-9372-74cbf4cb5850', 'Ventas', '/dashboard/sales', 'shopping-cart', 8, NULL),
     ('91fd5da2-4cee-441a-af46-c293b7e25566', 'Ordenes', '/dashboard/orders', 'package', 7, NULL),
-    ('f2effea2-23e2-4360-8a1d-29999cd8b813', 'Caja registradora', '/dashboard/cash', 'cash-register', 6, NULL),
+    ('f2effea2-23e2-4360-8a1d-29999cd8b813', 'Caja registradora', '/dashboard/cash', 'coins', 6, NULL),
     ('3215a491-958b-43f9-80b6-b60b7c99b83e', 'Tiendas', '/dashboard/stores', 'store', 5, NULL),
     ('68bc02cf-c9de-46b6-ae7f-583d951906f8', 'Empleados', '/dashboard/employees', 'users', 4, NULL);
 
