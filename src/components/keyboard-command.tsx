5b4c4a868fc187f791f4439add5a86208525ab08
Carlos Pavajeau <cantte098@gmail.com>
2024-07-05 16:00:44 -0500

    chore: Refactor KeyboardCommand component to use arrow function syntax

--- src/components/keyboard-command.tsx
+++ src/components/keyboard-command.tsx
@@ -2,10 +2,12 @@ type Props = {
   command: string
 }
 
-export default function KeyboardCommand({ command }: Props) {
+const KeyboardCommand = ({ command }: Props) => {
   return (
     <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-medium font-mono text-[10px] text-muted-foreground opacity-100">
       <span className="text-xs">{command}</span>
     </kbd>
   )
 }
+
+export default KeyboardCommand
