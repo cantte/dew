b319b27998ed9e40204ee92a3c66d8e7f064dd9c
Carlos Pavajeau <cantte098@gmail.com>
2024-07-11 11:24:16 -0500

    feat: Add PreventNavigation component to order and sale forms

--- src/app/(dashboard)/sales/create/form.tsx
+++ src/app/(dashboard)/sales/create/form.tsx
@@ -7,6 +7,7 @@ import type { z } from 'zod'
 import CheckoutStep from '~/app/(dashboard)/sales/create/checkout-step'
 import SelectCustomerStep from '~/app/(dashboard)/sales/create/select-customer-step'
 import SelectProductsStep from '~/app/(dashboard)/sales/create/select-products-step'
+import { PreventNavigation } from '~/components/prevent-navigation'
 import { Form } from '~/components/ui/form'
 import { useToast } from '~/components/ui/use-toast'
 import { createSaleInput } from '~/server/api/schemas/sales'
@@ -74,34 +75,45 @@ const CreateSaleForm = ({ storeId, suggestions }: Props) => {
     }
   }, [createSale.isSuccess])
 
+  const items = form.watch('items').length
+  const prevent = form.formState.isDirty || items > 0
+
   return (
-    <Form {...form}>
-      <form
-        className="flex min-h-[calc(100vh-20rem)] flex-col space-y-4"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        {
+    <>
+      <PreventNavigation
+        prevent={prevent}
+        backHref="/dashboard"
+        reset={form.reset}
+      />
+
+      <Form {...form}>
+        <form
+          className="flex min-h-[calc(100vh-20rem)] flex-col space-y-4"
+          onSubmit={form.handleSubmit(onSubmit)}
+        >
           {
-            'select-products': (
-              <SelectProductsStep
-                onContinue={handleFinishSelectProducts}
-                suggestions={suggestions}
-              />
-            ),
-            'select-customer': (
-              <SelectCustomerStep onContinue={handleFinishSelectCustomer} />
-            ),
-            checkout: (
-              <CheckoutStep
-                isCreating={createSale.isPending}
-                selectedProducts={selectedProducts}
-                customer={selectedCustomer}
-              />
-            ),
-          }[step]
-        }
-      </form>
-    </Form>
+            {
+              'select-products': (
+                <SelectProductsStep
+                  onContinue={handleFinishSelectProducts}
+                  suggestions={suggestions}
+                />
+              ),
+              'select-customer': (
+                <SelectCustomerStep onContinue={handleFinishSelectCustomer} />
+              ),
+              checkout: (
+                <CheckoutStep
+                  isCreating={createSale.isPending}
+                  selectedProducts={selectedProducts}
+                  customer={selectedCustomer}
+                />
+              ),
+            }[step]
+          }
+        </form>
+      </Form>
+    </>
   )
 }
 
