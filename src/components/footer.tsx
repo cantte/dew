8dfa9d06ce35a142777f2ee5df4e11a2ab044c33
Carlos Pavajeau <cantte098@gmail.com>
2024-07-12 16:23:12 -0500

    refactor: Update footer component with ThemeToggle and link changes

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,21 +1,30 @@
 import Link from 'next/link'
+import { ThemeToggle } from '~/components/theme-toggle'
 
 const Footer = () => {
   return (
-    <footer className="flex h-[80px] flex-col items-center justify-center text-center">
-      <span className="inline-flex items-center gap-2 font-normal text-muted-foreground text-sm">
-        {new Date().getFullYear()} © dew
-      </span>
-      <span className="inline-flex items-center gap-2 font-normal text-muted-foreground text-sm">
-        Creado con <span className="text-red-500">❤</span> por
-        <Link
-          href="https://github.com/cantte"
-          target="_blank"
-          className="text-foreground transition-colors duration-200 hover:text-foreground/80"
-        >
-          cantte
-        </Link>
-      </span>
+    <footer className="flex items-center justify-center border-t py-4">
+      <div className="flex w-full max-w-7xl items-center justify-between">
+        <div className="flex flex-col items-center">
+          <span className="inline-flex items-center gap-2 font-normal text-muted-foreground text-sm">
+            {new Date().getFullYear()} © dew
+          </span>
+          <span className="inline-flex items-center gap-2 font-normal text-muted-foreground text-sm">
+            Creado con <span className="text-red-500">❤</span> por
+            <Link
+              href="https://www.cantte.com/"
+              target="_blank"
+              className="text-foreground transition-colors duration-200 hover:text-foreground/80"
+            >
+              cantte
+            </Link>
+          </span>
+        </div>
+
+        <div>
+          <ThemeToggle />
+        </div>
+      </div>
     </footer>
   )
 }
