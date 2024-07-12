d740a28542e4afd12aaabb4423f629277258e991
Carlos Pavajeau <cantte098@gmail.com>
2024-07-12 17:07:11 -0500

    refactor: remove lg height

--- src/components/default-layout.tsx
+++ src/components/default-layout.tsx
@@ -11,7 +11,7 @@ const DefaultLayout = ({ children }: Props) => {
   return (
     <div className="min-h-screen w-full overflow-hidden">
       <div className="flex min-h-screen flex-col">
-        <header className="flex h-14 items-center justify-between gap-4 border-b px-6 lg:h-[60px]">
+        <header className="flex h-14 items-center justify-between gap-4 border-b px-6">
           <Link className="mr-4 flex items-center gap-2" href="/">
             <span className="font-semibold text-lg">dew</span>
             <Badge>beta</Badge>
