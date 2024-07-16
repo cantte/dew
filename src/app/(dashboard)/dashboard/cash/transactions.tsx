55a48a14a1bd786bbbfe0b51bdebc62c6d15b80b
Carlos Pavajeau <cantte098@gmail.com>
2024-07-16 11:27:13 -0500

    refactor: Add tooltip to display transaction date and employee

--- src/app/(dashboard)/dashboard/cash/transactions.tsx
+++ src/app/(dashboard)/dashboard/cash/transactions.tsx
@@ -1,8 +1,15 @@
 'use client'
 
+import { Calendar, User } from 'lucide-react'
 import { Badge } from '~/components/ui/badge'
 import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card'
 import { ScrollArea } from '~/components/ui/scroll-area'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '~/components/ui/tooltip'
 import type { RouterOutputs } from '~/trpc/shared'
 
 type Props = {
@@ -25,7 +32,7 @@ const CashRegisterTransactions = ({ transactions }: Props) => {
           return (
             <Card key={transaction.id} className="mb-2 rounded shadow-none">
               <CardHeader className="flex flex-row items-center justify-between space-y-0 px-6 pt-2 pb-1">
-                <CardTitle className="font-bold text-2xl">
+                <CardTitle className="font-bold text-xl">
                   {Intl.NumberFormat('es-CO', {
                     style: 'currency',
                     currency: 'COP',
@@ -41,18 +48,42 @@ const CashRegisterTransactions = ({ transactions }: Props) => {
                 </Badge>
               </CardHeader>
               <CardContent className="px-6 pb-2">
-                <div className="text-muted-foreground text-sm">
-                  {Intl.DateTimeFormat('es-CO', {
-                    dateStyle: 'medium',
-                    timeStyle: 'short',
-                  }).format(new Date(transaction.createdAt))}
-                </div>
+                <TooltipProvider>
+                  <div className="grid gap-1.5">
+                    <div className="flex items-center">
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <Calendar className="mr-1 inline-block h-4 w-4" />
+                        </TooltipTrigger>
 
-                <div className="text-muted-foreground text-sm">
-                  <Badge variant="secondary" className="mt-2">
-                    {transaction.user.name}
-                  </Badge>
-                </div>
+                        <TooltipContent>
+                          <span>Fecha de la transacción</span>
+                        </TooltipContent>
+                      </Tooltip>
+                      <span className="text-muted-foreground text-sm">
+                        {Intl.DateTimeFormat('es-CO', {
+                          dateStyle: 'medium',
+                          timeStyle: 'short',
+                        }).format(new Date(transaction.createdAt))}
+                      </span>
+                    </div>
+
+                    <div className="flex items-center">
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <User className="mr-1 inline-block h-4 w-4" />
+                        </TooltipTrigger>
+
+                        <TooltipContent>
+                          <span>Empleado que realizó la transacción</span>
+                        </TooltipContent>
+                      </Tooltip>
+                      <span className="text-muted-foreground text-sm">
+                        {transaction.employee}
+                      </span>
+                    </div>
+                  </div>
+                </TooltipProvider>
               </CardContent>
             </Card>
           )
