8a4ffd93664d2aa56fad68936208cc8708dcdc25
Carlos Pavajeau <cantte098@gmail.com>
2024-07-20 15:30:16 -0500

    feat: Add makeCashMovement function for cash register transactions

--- src/server/api/schemas/cashRegisters.ts
+++ src/server/api/schemas/cashRegisters.ts
@@ -15,3 +15,10 @@ export const listCashRegisterTransactionsInput = z.object({
   from: z.coerce.date(),
   to: z.coerce.date(),
 })
+
+export const makeCashMovementInput = z.object({
+  storeId: z.string().min(1).max(36),
+  userId: z.string().min(1).max(36),
+  type: z.enum(['IN', 'OUT']),
+  amount: z.coerce.number().min(1),
+})
