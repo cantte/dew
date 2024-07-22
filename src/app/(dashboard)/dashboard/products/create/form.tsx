c5c060d7e4694db978e5675c0b2e9160279ebe28
Carlos Pavajeau <cantte098@gmail.com>
2024-07-22 11:23:22 -0500

    feat: new register product ui

--- src/app/(dashboard)/dashboard/products/create/form.tsx
+++ src/app/(dashboard)/dashboard/products/create/form.tsx
@@ -3,11 +3,11 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ReloadIcon } from '@radix-ui/react-icons'
 import { useDebounce } from '@uidotdev/usehooks'
-import { useEffect, useMemo } from 'react'
+import { Tag } from 'lucide-react'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
 import type { z } from 'zod'
-import MultiSelectStore from '~/components/stores/multi-select-store'
 import { Badge } from '~/components/ui/badge'
 import { Button } from '~/components/ui/button'
 
@@ -24,44 +24,40 @@ import { Input } from '~/components/ui/input'
 import { Textarea } from '~/components/ui/textarea'
 import { useToast } from '~/components/ui/use-toast'
 import { createProductInput } from '~/server/api/schemas/products'
+import { formatToCurrency } from '~/text/format'
 import { api } from '~/trpc/react'
 import type { RouterOutputs } from '~/trpc/shared'
 
 type Props = {
-  storeId: string
-
-  stores: RouterOutputs['store']['list']
+  store: NonNullable<RouterOutputs['store']['findCurrent']>
 }
 
 type FormValues = z.infer<typeof createProductInput>
 
-const CreateProductForm = ({ storeId, stores }: Props) => {
+export const CreateProductForm = ({ store }: Props) => {
   const form = useForm<FormValues>({
     resolver: zodResolver(createProductInput),
     defaultValues: {
       code: '',
       name: '',
-      stores: [storeId],
+      storeId: store.id,
+      stock: 0,
+      quantity: 0,
     },
   })
 
-  const currentStore = useMemo(
-    () => stores.find((store) => store.id === storeId),
-    [stores, storeId],
-  )
-
-  const selectedStores = form.watch('stores', [])
-  const setSelectedStores = (value: Array<string>) =>
-    form.setValue('stores', value)
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: not needed
-  useEffect(() => {
-    if (!currentStore) {
-      return
-    }
-
-    setSelectedStores([currentStore.id])
-  }, [currentStore])
+  const resetForm = () => {
+    form.reset({
+      code: '',
+      name: '',
+      description: '',
+      purchasePrice: 0,
+      salePrice: 0,
+      stock: 0,
+      quantity: 0,
+      storeId: store.id,
+    })
+  }
 
   const createProduct = api.product.create.useMutation()
 
@@ -74,9 +70,7 @@ const CreateProductForm = ({ storeId, stores }: Props) => {
         description: 'El producto se creó con éxito.',
       })
 
-      form.reset({
-        stores: [storeId],
-      })
+      resetForm()
     }
   }, [createProduct.isSuccess])
 
@@ -107,143 +101,213 @@ const CreateProductForm = ({ storeId, stores }: Props) => {
         message: 'El código ya existe',
       })
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [exists, error])
 
+  const salePrice = form.watch('salePrice')
+  const purchasePrice = form.watch('purchasePrice')
+
   return (
-    <Form {...form}>
-      <form
-        className="flex flex-col space-y-4"
-        onSubmit={form.handleSubmit(onSubmit)}
-      >
-        <FormField
-          control={form.control}
-          name="code"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Código</FormLabel>
-              <FormControl>
-                <Input type="text" autoFocus {...field} />
-              </FormControl>
+    <div className="flex min-h-[calc(100vh-20rem)] w-full flex-col space-y-4">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="grid grow grid-cols-1 gap-4 md:grid-cols-3">
+            <div className="rounded border p-4 md:col-span-2">
+              <div className="grid gap-4">
+                <div>
+                  <p className="font-bold text-lg">Información general</p>
+                </div>
 
-              <FormDescription>
-                Puedes usar el código de barras del producto. Escanéalo con un
-                lector de códigos de barras.
-              </FormDescription>
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Nombre</FormLabel>
+                      <FormControl>
+                        <Input type="text" autoFocus {...field} />
+                      </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-
-        <FormField
-          control={form.control}
-          name="stores"
-          render={() => (
-            <FormItem>
-              <div>
-                <MultiSelectStore
-                  stores={stores}
-                  selectedStores={selectedStores}
-                  onSelectedChange={setSelectedStores}
+                      <FormMessage />
+                    </FormItem>
+                  )}
                 />
 
-                <div className="mt-1.5 flex flex-row items-center justify-between">
-                  {selectedStores.length > 0 && (
-                    <div>
-                      <span className="text-muted-foreground text-xs">
-                        Tiendas seleccionadas
-                      </span>
-                      <ul className="mt-1 flex flex-row space-x-2">
-                        {selectedStores.map((store) => (
-                          <Badge variant="outline" key={store}>
-                            {stores?.find((s) => s.id === store)?.name}
-                          </Badge>
-                        ))}
-                      </ul>
-                    </div>
+                <FormField
+                  control={form.control}
+                  name="description"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Descripción</FormLabel>
+                      <FormControl>
+                        <Textarea className="resize-none" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
                   )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="code"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Código de barras</FormLabel>
+                      <FormControl>
+                        <Input type="text" {...field} />
+                      </FormControl>
+
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <div>
+                  <p className="font-bold text-lg">Precios</p>
+                </div>
+
+                <div>
+                  <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                    <FormField
+                      control={form.control}
+                      name="purchasePrice"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Precio de compra</FormLabel>
+                          <FormControl>
+                            <Input type="number" {...field} />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="salePrice"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Precio de venta</FormLabel>
+                          <FormControl>
+                            <Input type="number" {...field} />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <p className="font-bold text-lg">Inventario</p>
+                  <span className="text-muted-foreground text-sm">
+                    No se llevará un control de inventario si el stock es 0.
+                  </span>
+                </div>
+
+                <div>
+                  <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+                    <FormField
+                      control={form.control}
+                      name="quantity"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Cantidad</FormLabel>
+                          <FormControl>
+                            <Input type="number" {...field} />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <FormField
+                      control={form.control}
+                      name="stock"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormLabel>Stock</FormLabel>
+                          <FormControl>
+                            <Input type="number" {...field} />
+                          </FormControl>
+
+                          <FormDescription>
+                            Recibirás una notificación cuando la cantidad de
+                            productos sea menor al stock.
+                          </FormDescription>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
                 </div>
               </div>
+            </div>
+            <div className="flex flex-col justify-between gap-4 rounded border p-4">
+              <div className="grid gap-4 text-sm">
+                <div>
+                  <Badge variant="secondary">{store.name}</Badge>
+                </div>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                <div className="flex flex-col items-center space-y-2">
+                  <Tag className="size-16 text-muted-foreground" />
 
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre</FormLabel>
-              <FormControl>
-                <Input type="text" {...field} />
-              </FormControl>
+                  <p className="text-base">
+                    {form.watch('name') || 'Nombre del producto'}
+                  </p>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                  <p className="text-muted-foreground text-sm">
+                    {formatToCurrency('es-CO', form.watch('salePrice') ?? 0)}
+                  </p>
+                </div>
 
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Descripción</FormLabel>
-              <FormControl>
-                <Textarea className="resize-none" {...field} />
-              </FormControl>
+                <div>
+                  <FormField
+                    control={form.control}
+                    name="reference"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Referencia</FormLabel>
+                        <FormControl>
+                          <Input type="text" {...field} />
+                        </FormControl>
 
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+                        <FormDescription>
+                          Código unico para indentificar tus productos
+                        </FormDescription>
 
-        <div>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-            <FormField
-              control={form.control}
-              name="purchasePrice"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Precio de compra</FormLabel>
-                  <FormControl>
-                    <Input type="number" {...field} />
-                  </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
 
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+                {salePrice > 0 && purchasePrice > 0 && (
+                  <div>
+                    <span className="font-medium text-muted-foreground text-sm">
+                      Ganancia aproximada por unidad:{' '}
+                      {formatToCurrency('es-CO', salePrice - purchasePrice)}
+                    </span>
+                  </div>
+                )}
+              </div>
 
-            <FormField
-              control={form.control}
-              name="salePrice"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Precio de venta</FormLabel>
-                  <FormControl>
-                    <Input type="number" {...field} />
-                  </FormControl>
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
+              <div className="flex flex-col space-y-2">
+                <Button type="submit" disabled={createProduct.isPending}>
+                  {createProduct.isPending && (
+                    <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                  Agregar
+                </Button>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <Button type="submit" disabled={createProduct.isPending}>
-          {createProduct.isPending && (
-            <ReloadIcon className="mr-2 h-4 w-4 animate-spin" />
-          )}
-          Agregar
-        </Button>
-      </form>
-    </Form>
+        </form>
+      </Form>
+    </div>
   )
 }
-
-export default CreateProductForm
