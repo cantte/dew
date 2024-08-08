81ae93706b59416c3e8c7886f84db13a4e4b776c
Carlos Pavajeau <cantte098@gmail.com>
2024-08-08 15:17:08 -0500

    refactor: Update employee creation page layout and form

--- src/app/(dashboard)/dashboard/employees/create/page.tsx
+++ src/app/(dashboard)/dashboard/employees/create/page.tsx
@@ -1,18 +1,10 @@
-import { redirect } from 'next/navigation'
 import CreateEmployeeForm from '~/app/(dashboard)/dashboard/employees/create/form'
 import BackButton from '~/components/back-button'
 import NotEnoughPermissions from '~/components/not-enough-permissions'
 import NotFoundStoreAlert from '~/components/stores/not-found.alert'
-import { getServerAuthSession } from '~/server/auth'
 import { api } from '~/trpc/server'
 
-const CreateEmployeePage = async () => {
-  const session = await getServerAuthSession()
-
-  if (!session) {
-    return redirect('/api/auth/signin')
-  }
-
+export default async function CreateEmployeePage() {
   const store = await api.store.findCurrent()
 
   if (!store) {
@@ -28,18 +20,10 @@ const CreateEmployeePage = async () => {
   }
 
   return (
-    <div className="flex w-full flex-col items-center">
-      <div className="w-full max-w-xl space-y-4">
-        <BackButton />
+    <div className="flex flex-col items-start space-y-4">
+      <BackButton />
 
-        <section className="flex flex-col gap-4">
-          <h1 className="font-bold text-xl">Agregar empleado</h1>
-
-          <CreateEmployeeForm storeId={store.id} />
-        </section>
-      </div>
+      <CreateEmployeeForm store={store} />
     </div>
   )
 }
-
-export default CreateEmployeePage
