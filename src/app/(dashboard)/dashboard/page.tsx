5a0bff36142891d8e73540066efa7d3cf6fb9b41
Carlos Pavajeau <cantte098@gmail.com>
2024-08-09 11:49:12 -0500

    refactor: load sale form on dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,105 +1,42 @@
-import { Suspense } from 'react'
-import { GeneralOverview } from '~/components/dashboard/general-overview'
-import { GeneralReport } from '~/components/dashboard/general-report'
-import LowStockProducts from '~/components/dashboard/low-stock-products'
-import MostSoldProducts from '~/components/dashboard/most-sold-products'
-import OrdersReport from '~/components/dashboard/orders-report'
-import SalesReport from '~/components/dashboard/sales-report'
-import OrdersOverview from '~/components/orders/overview'
-import SalesOverview from '~/components/sales/overview'
+import CreateSaleForm from '~/app/(dashboard)/sales/create/form'
+import NotEnoughPermissions from '~/components/not-enough-permissions'
 import NotFoundStoreAlert from '~/components/stores/not-found.alert'
-import { Skeleton } from '~/components/ui/skeleton'
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '~/components/ui/tabs'
 import { api } from '~/trpc/server'
 
-const DashboardPage = async () => {
+export default async function DashboardPage() {
   const store = await api.store.findCurrent()
   if (!store) {
     return <NotFoundStoreAlert />
   }
 
+  const hasPermissions = await api.rbac.checkPermissions({
+    permissions: ['sale:create', 'product:view'],
+  })
+
+  if (!hasPermissions) {
+    return <NotEnoughPermissions />
+  }
+
+  const suggestions = await api.product.suggestions({
+    storeId: store.id,
+  })
+
+  const products = await api.product.forSale({
+    storeId: store.id,
+  })
+
+  const employees = await api.employee.byStore({
+    storeId: store.id,
+  })
+
   return (
-    <>
-      <div className="space-y-2">
-        <span className="font-semibold tracking-tight">Reporte general</span>
-
-        <div className="grid gap-4">
-          <Suspense
-            fallback={<Skeleton className="h-5 w-full max-w-[450px]" />}
-          >
-            <GeneralOverview />
-          </Suspense>
-
-          <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
-            <Suspense fallback={<SalesReportFallback />}>
-              <GeneralReport />
-            </Suspense>
-          </div>
-        </div>
-      </div>
-
-      <div className="space-y-2">
-        <span className="font-semibold tracking-tight">
-          Reporte de ventas y ordenes
-        </span>
-
-        <Tabs defaultValue="sales">
-          <TabsList>
-            <TabsTrigger value="sales">Ventas</TabsTrigger>
-            <TabsTrigger value="orders">Ordenes</TabsTrigger>
-          </TabsList>
-          <TabsContent value="sales">
-            <div className="grid gap-4">
-              <Suspense
-                fallback={<Skeleton className="h-5 w-full max-w-[450px]" />}
-              >
-                <SalesOverview />
-              </Suspense>
-
-              <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
-                <Suspense fallback={<SalesReportFallback />}>
-                  <SalesReport />
-                </Suspense>
-              </div>
-            </div>
-          </TabsContent>
-
-          <TabsContent value="orders">
-            <div className="grid gap-4">
-              <Suspense
-                fallback={<Skeleton className="h-5 w-full max-w-[450px]" />}
-              >
-                <OrdersOverview />
-              </Suspense>
-
-              <div className="grid gap-4 sm:grid-cols-2 xl:grid-cols-2">
-                <Suspense fallback={<SalesReportFallback />}>
-                  <OrdersReport />
-                </Suspense>
-              </div>
-            </div>
-          </TabsContent>
-        </Tabs>
-      </div>
-
-      <Suspense fallback={<Skeleton className="h-10 w-full" />}>
-        <MostSoldProducts />
-      </Suspense>
-
-      <Suspense fallback={<Skeleton className="h-10 w-full" />}>
-        <LowStockProducts />
-      </Suspense>
-    </>
+    <div className="flex w-full flex-col items-start space-y-4">
+      <CreateSaleForm
+        store={store}
+        employees={employees}
+        products={products}
+        suggestions={suggestions}
+      />
+    </div>
   )
 }
-
-const SalesReportFallback = () => {
-  return (
-    <>
-      <Skeleton className="h-[200px] w-full" />
-      <Skeleton className="h-[200px] w-full" />
-    </>
-  )
-}
-
-export default DashboardPage
