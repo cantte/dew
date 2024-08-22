51c97ef157fcc20c2b8c03374e7d29c0be7db17e
Carlos Pavajeau <cantte098@gmail.com>
2024-08-22 12:27:46 -0500

    feat: update yearly overview fallback

--- src/components/sales/yearly-overview.tsx
+++ src/components/sales/yearly-overview.tsx
@@ -81,5 +81,61 @@ export const YearlySalesOverview = async () => {
 }
 
 export const YearlySalesOverviewFallback = () => {
-  return <Skeleton className="h-5 w-full max-w-[450px]" />
+  return (
+    <div className="grid gap-2 grid-cols-2 md:grid-cols-4 w-full border rounded">
+      <div className="p-2 flex justify-between">
+        <div className="p-2">
+          <div className="flex space-x-2 items-center">
+            <ShoppingCart className="size-3.5 text-muted-foreground" />
+            <p className="text-sm font-medium">Ventas totales</p>
+          </div>
+          <p className="text-lg font-semibold">
+            <Skeleton className="w-16 h-6" />
+          </p>
+        </div>
+
+        <Separator orientation="vertical" />
+      </div>
+
+      <div className="p-2 flex justify-between">
+        <div className="p-2">
+          <div className="flex space-x-2 items-center">
+            <Tag className="size-3.5 text-muted-foreground" />
+            <p className="text-sm font-medium">Productos vendidos</p>
+          </div>
+          <p className="text-lg font-semibold">
+            <Skeleton className="w-16 h-6" />
+          </p>
+        </div>
+
+        <Separator orientation="vertical" />
+      </div>
+
+      <div className="p-2 flex justify-between">
+        <div className="p-2">
+          <div className="flex space-x-2 items-center">
+            <CircleDollarSign className="size-3.5 text-muted-foreground" />
+            <p className="text-sm font-medium">Ingesos generados</p>
+          </div>
+          <p className="text-lg font-semibold">
+            <Skeleton className="w-16 h-6" />
+          </p>
+        </div>
+
+        <Separator orientation="vertical" />
+      </div>
+
+      <div className="p-2 flex justify-between">
+        <div className="p-2">
+          <div className="flex space-x-2 items-center">
+            <CircleDollarSign className="size-3.5 text-muted-foreground" />
+            <p className="text-sm font-medium">Ganancias generadas</p>
+          </div>
+          <p className="text-lg font-semibold">
+            <Skeleton className="w-16 h-6" />
+          </p>
+        </div>
+      </div>
+    </div>
+  )
 }
