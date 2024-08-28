c550e581c231b5375888642b4d090ce22ffd6a2c
Carlos Pavajeau <cantte098@gmail.com>
2024-08-28 16:46:58 -0500

    refactor: add searchPermissions router to rbacRouter

--- src/server/api/routers/rbac/index.ts
+++ src/server/api/routers/rbac/index.ts
@@ -1,5 +1,6 @@
 import authedProcedure from '~/server/api/procedures/authed'
 import checkPermissions from '~/server/api/routers/rbac/checkPermissions'
+import { searchPermissions } from '~/server/api/routers/rbac/search-permissions'
 import { checkPermissionsInput } from '~/server/api/schemas/rbac'
 import { router } from '~/server/api/trpc'
 
@@ -8,6 +9,10 @@ const rbacRouter = router({
     .input(checkPermissionsInput)
     .query(async ({ ctx, input }) => {
       return await checkPermissions({ ctx, input })
+    }),
+  list: authedProcedure
+    .query(async ({ ctx }) => {
+      return await searchPermissions({ ctx })
     }),
 })
 
