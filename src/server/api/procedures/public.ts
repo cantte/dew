7d6fbf8ceb8dbc79bbb19ad184d029dd950bf8cc
Carlos Pavajeau <cantte098@gmail.com>
2024-08-28 16:36:43 -0500

    refactor: add performance middleware to API procedures

--- src/server/api/procedures/public.ts
+++ src/server/api/procedures/public.ts
@@ -1,7 +1,11 @@
 import captureErrors from '~/server/api/middlewares/captureErrors'
+import { performanceMiddleware } from '~/server/api/middlewares/performance'
 import rateLimit from '~/server/api/middlewares/ratelimit'
 import { trpcContext } from '~/server/api/trpc'
 
-const publicProcedure = trpcContext.procedure.use(captureErrors).use(rateLimit)
+const publicProcedure = trpcContext.procedure
+  .use(captureErrors)
+  .use(performanceMiddleware)
+  .use(rateLimit)
 
 export default publicProcedure
