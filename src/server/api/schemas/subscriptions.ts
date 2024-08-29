6e932dd816ee50886c5a0117d6b8bd7a5fc25ec3
Carlos Pavajeau <cantte098@gmail.com>
2024-08-29 10:34:02 -0500

    feat: add updateSubscription endpoint to subscriptions router

--- src/server/api/schemas/subscriptions.ts
+++ src/server/api/schemas/subscriptions.ts
@@ -3,3 +3,8 @@ import { z } from 'zod'
 export const createSubscriptionInput = z.object({
   planId: z.string(),
 })
+
+export const updateSubscriptionInput = z.object({
+  id: z.string(),
+  status: z.enum(['active', 'past_due', 'inactive']),
+})
