c9bcc1795eab58ca0a1ab1b54376f6fe3e8c1911
Carlos Pavajeau <cantte098@gmail.com>
2024-09-01 18:21:24 -0500

    feat: add create user subscription

--- src/server/api/routers/subscriptions/create.ts
+++ src/server/api/routers/subscriptions/create.ts
@@ -1,9 +1,13 @@
-import { addDays } from 'date-fns'
+import { toDate } from 'date-fns'
 import type { TypeOf } from 'zod'
 import uuid from '~/lib/uuid'
 import type { TRPCAuthedContext } from '~/server/api/procedures/authed'
+import { createUserPaymentInfo } from '~/server/api/routers/subscriptions/create-user-payment-info'
+import { findUserPaymentInfo } from '~/server/api/routers/subscriptions/find-user-payment-info'
 import type { createSubscriptionInput } from '~/server/api/schemas/subscriptions'
 import { subscriptions } from '~/server/db/schema'
+import { ePaycoChargeSubscription } from '~/server/epayco/charge-subscription'
+import { ePaycoCreateSubscription } from '~/server/epayco/create-subscription'
 
 type Options = {
   ctx: TRPCAuthedContext
@@ -11,15 +15,52 @@ type Options = {
 }
 
 export const createSubscription = async ({ ctx, input }: Options) => {
-  await ctx.db.transaction(async (tx) => {
-    const periodEnd = addDays(new Date(), 30)
+  const ip = ctx.headers.get('x-forwarded-for') || ''
+  const userId = ctx.session.user.id
 
-    tx.insert(subscriptions).values({
-      ...input,
-      id: uuid(),
-      status: 'active',
-      periodEnd,
-      userId: ctx.session.user.id,
-    })
+  let userPayment = await findUserPaymentInfo({ ctx })
+
+  if (!userPayment) {
+    userPayment = await createUserPaymentInfo({ ctx, input })
+  }
+
+  if (!userPayment.customerId || !userPayment.cardToken) {
+    throw new Error('Failed to create user payment info')
+  }
+
+  const createSubscriptionResponse = await ePaycoCreateSubscription({
+    id_plan: input.planId,
+    customer: userPayment.customerId,
+    token_card: userPayment.cardToken,
+    doc_type: input.customer.docType,
+    doc_number: input.customer.docNumber,
+  })
+
+  if (!createSubscriptionResponse.success) {
+    throw new Error('Failed to create subscription')
+  }
+
+  const chargeSubscriptionResponse = await ePaycoChargeSubscription({
+    id_plan: input.planId,
+    customer: userPayment.customerId,
+    token_card: userPayment.cardToken,
+    doc_type: input.customer.docType,
+    doc_number: input.customer.docNumber,
+    ip,
+  })
+
+  if (!chargeSubscriptionResponse.success) {
+    throw new Error('Failed to charge subscription')
+  }
+
+  await ctx.db.insert(subscriptions).values({
+    id: uuid(),
+    status:
+      chargeSubscriptionResponse.subscription.status === 'active'
+        ? 'active'
+        : 'inactive',
+    periodEnd: toDate(chargeSubscriptionResponse.subscription.periodEnd),
+    planId: input.planId,
+    userId,
   })
 }
--- src/server/api/schemas/subscriptions.ts
+++ src/server/api/schemas/subscriptions.ts
@@ -1,7 +1,21 @@
 import { z } from 'zod'
 
 export const createSubscriptionInput = z.object({
-  planId: z.string(),
+  planId: z.enum(['dew_mensual', 'dew_anual']),
+  card: z.object({
+    number: z.string(),
+    expMonth: z.string(),
+    expYear: z.string(),
+    cvc: z.string(),
+  }),
+  customer: z.object({
+    name: z.string(),
+    lastName: z.string(),
+    email: z.string(),
+    phone: z.string(),
+    docType: z.string(),
+    docNumber: z.string(),
+  }),
 })
 
 export const updateSubscriptionInput = z.object({
