e49c31eb1353e5f300e67e98c59ce99c5917d3d7
Carlos Pavajeau <cantte098@gmail.com>
2024-09-02 10:18:29 -0500

    feat(subscriptions): add findUserSubscription function

---
+++ src/server/api/routers/subscriptions/find.ts
@@ -0,0 +1,21 @@
+import { eq } from 'drizzle-orm'
+import type { TRPCAuthedContext } from '~/server/api/procedures/authed'
+import { subscriptions } from '~/server/db/schema'
+
+type Options = {
+  ctx: TRPCAuthedContext
+}
+
+export const findUserSubscription = async ({ ctx }: Options) => {
+  const userId = ctx.session.user.id
+
+  const [subscription] = await ctx.db
+    .select({
+      id: subscriptions.id,
+      status: subscriptions.status,
+    })
+    .from(subscriptions)
+    .where(eq(subscriptions.userId, userId))
+
+  return subscription
+}
--- src/server/api/routers/subscriptions/index.ts
+++ src/server/api/routers/subscriptions/index.ts
@@ -1,5 +1,6 @@
 import authedProcedure from '~/server/api/procedures/authed'
 import { createSubscription } from '~/server/api/routers/subscriptions/create'
+import { findUserSubscription } from '~/server/api/routers/subscriptions/find'
 import { findUserTrial } from '~/server/api/routers/subscriptions/find-trial'
 import { createSubscriptionInput } from '~/server/api/schemas/subscriptions'
 import { router } from '~/server/api/trpc'
@@ -13,4 +14,7 @@ export const subscriptionsRouter = router({
     .mutation(async ({ ctx, input }) => {
       await createSubscription({ ctx, input })
     }),
+  find: authedProcedure.query(async ({ ctx }) => {
+    return await findUserSubscription({ ctx })
+  }),
 })
