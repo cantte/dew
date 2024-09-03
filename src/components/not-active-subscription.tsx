99e9122441713f84baf3836e310caae4123e4e6e
Carlos Pavajeau <cantte098@gmail.com>
2024-09-03 10:54:02 -0500

    refactor: Update NotActiveSubscription component UI and content

--- src/components/not-active-subscription.tsx
+++ src/components/not-active-subscription.tsx
@@ -1,4 +1,4 @@
-import { Info } from 'lucide-react'
+import { TriangleAlert } from 'lucide-react'
 import NextLink from 'next/link'
 import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert'
 import { Button } from '~/components/ui/button'
@@ -6,17 +6,27 @@ import { Button } from '~/components/ui/button'
 export const NotActiveSubscription = () => {
   return (
     <Alert>
-      <Info className="h-4 w-4 text-muted-foreground" />
-      <AlertTitle>Suscripción no activa</AlertTitle>
-      <AlertDescription>
-        No tiene una suscripción activa o en periodo de prueba. Por favor,
-        active una suscripción para continuar.
-        <br />
-        <Button asChild size="sm" className="mt-2">
-          <NextLink href={`/dashboard/account`}>
-            Ir a configuración de cuenta
-          </NextLink>
-        </Button>
+      <div className="flex items-center space-x-2">
+        <span className="rounded-full bg-warning/10 p-1">
+          <TriangleAlert className="size-3 text-warning" />
+        </span>
+        <AlertTitle className="mb-0">Suscripción no activa</AlertTitle>
+      </div>
+      <AlertDescription className="mt-2">
+        <div className="grid gap-4">
+          <span>
+            No tiene una suscripción activa o en periodo de prueba. Por favor,
+            active una suscripción para continuar.
+          </span>
+
+          <div>
+            <Button asChild>
+              <NextLink href={`/dashboard/account`}>
+                Ir a configuración de cuenta
+              </NextLink>
+            </Button>
+          </div>
+        </div>
       </AlertDescription>
     </Alert>
   )
