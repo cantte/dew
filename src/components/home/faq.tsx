28705f60421fef369cb6ea59f1308015a907926e
Carlos Pavajeau <cantte098@gmail.com>
2024-09-05 13:38:32 -0500

    Refactor: Update FAQSection component in home page

--- src/components/home/faq.tsx
+++ src/components/home/faq.tsx
@@ -5,70 +5,58 @@ import {
   AccordionTrigger,
 } from '~/components/ui/accordion'
 
+const faq = [
+  {
+    id: 'faq-1',
+    title: '¿Qué es Dew y para qué sirve?',
+    content:
+      'Dew es una aplicación web que funciona como un sistema de ventas POS (Punto de Venta). Permite a los usuarios manejar inventario, realizar ventas rápidas, emitir facturación electrónica, y generar reportes de ventas mensuales y anuales.',
+  },
+  {
+    id: 'faq-2',
+    title: '¿Cuánto cuesta usar Dew?',
+    content:
+      'Actualmente, Dew ofrece un único plan con un costo de 20,000 pesos colombianos por mes. Este plan incluye todas las funciones disponibles en la plataforma.',
+  },
+  {
+    id: 'faq-3',
+    title: '¿Cómo puedo generar un reporte de ventas?',
+    content:
+      'Puedes generar un reporte de ventas desde la página de "Reportes" en Dew. Solo tienes que seleccionar el período que deseas (mensual o anual) y el sistema generará el reporte automáticamente.',
+  },
+  {
+    id: 'faq-4',
+    title: '¿Es segura mi información en Dew?',
+    content:
+      'Sí, tomamos muy en serio la seguridad de su información. Actualmente, todos los datos están protegidos, y estamos trabajando en agregar autenticación en dos pasos y soporte multiusuario mediante permisos para mejorar aún más la seguridad.',
+  },
+  {
+    id: 'faq-5',
+    title: '¿Puedo utilizar Dew en mi teléfono móvil o tableta?',
+    content:
+      'Sí, Dew es una aplicación web accesible desde cualquier navegador, lo que significa que puedes utilizarla en tu computadora, tableta o teléfono móvil sin problemas.',
+  },
+  {
+    id: 'faq-6',
+    title: '¿Cómo funciona la facturación electrónica en Dew?',
+    content:
+      'Dew permite generar facturas electrónicas de manera rápida y sencilla al completar una venta. Las facturas pueden ser enviadas directamente al correo electrónico del cliente o descargadas en formato PDF.',
+  },
+]
+
 export const FAQSection = () => {
   return (
     <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="faq-1">
-        <AccordionTrigger>¿Qué es Dew y para qué sirve?</AccordionTrigger>
-        <AccordionContent>
-          Dew es una aplicación web que funciona como un sistema de ventas POS
-          (Punto de Venta). Permite a los usuarios manejar inventario, realizar
-          ventas rápidas, emitir facturación electrónica, y generar reportes de
-          ventas mensuales y anuales.
-        </AccordionContent>
-      </AccordionItem>
-
-      <AccordionItem value="faq-2">
-        <AccordionTrigger>¿Cuánto cuesta usar Dew?</AccordionTrigger>
-        <AccordionContent>
-          Actualmente, Dew ofrece un único plan con un costo de 20,000 pesos
-          colombianos por mes. Este plan incluye todas las funciones disponibles
-          en la plataforma.
-        </AccordionContent>
-      </AccordionItem>
-
-      <AccordionItem value="faq-3">
-        <AccordionTrigger>
-          ¿Cómo puedo generar un reporte de ventas?
-        </AccordionTrigger>
-        <AccordionContent>
-          Puedes generar un reporte de ventas desde la página de "Reportes" en
-          Dew. Solo tienes que seleccionar el período que deseas (mensual o
-          anual) y el sistema generará el reporte automáticamente.
-        </AccordionContent>
-      </AccordionItem>
-
-      <AccordionItem value="faq-4">
-        <AccordionTrigger>¿Es segura mi información en Dew?</AccordionTrigger>
-        <AccordionContent>
-          Sí, tomamos muy en serio la seguridad de su información. Actualmente,
-          todos los datos están protegidos, y estamos trabajando en agregar
-          autenticación en dos pasos y soporte multiusuario mediante permisos
-          para mejorar aún más la seguridad.
-        </AccordionContent>
-      </AccordionItem>
-
-      <AccordionItem value="faq-5">
-        <AccordionTrigger>
-          ¿Puedo utilizar Dew en mi teléfono móvil o tableta?
-        </AccordionTrigger>
-        <AccordionContent>
-          Sí, Dew es una aplicación web accesible desde cualquier navegador, lo
-          que significa que puedes utilizarla en tu computadora, tableta o
-          teléfono móvil sin problemas.
-        </AccordionContent>
-      </AccordionItem>
-
-      <AccordionItem value="faq-6">
-        <AccordionTrigger>
-          ¿Cómo funciona la facturación electrónica en Dew?
-        </AccordionTrigger>
-        <AccordionContent>
-          Dew permite generar facturas electrónicas de manera rápida y sencilla
-          al completar una venta. Las facturas pueden ser enviadas directamente
-          al correo electrónico del cliente o descargadas en formato PDF.
-        </AccordionContent>
-      </AccordionItem>
+      {faq.map(({ id, title, content }) => (
+        <AccordionItem key={id} value={id}>
+          <AccordionTrigger>
+            <span className="font-semibold text-xl">{title}</span>
+          </AccordionTrigger>
+          <AccordionContent>
+            <p>{content}</p>
+          </AccordionContent>
+        </AccordionItem>
+      ))}
     </Accordion>
   )
 }
