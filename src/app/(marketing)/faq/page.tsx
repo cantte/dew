fded92e2e20dba9e447197a97262c7d2d1d46ca4
Carlos Pavajeau <cantte098@gmail.com>
2024-09-05 13:38:37 -0500

    Refactor: Update FAQSection component in home page layout and content

--- src/app/(marketing)/faq/page.tsx
+++ src/app/(marketing)/faq/page.tsx
@@ -4,10 +4,14 @@ import { PageWrapper } from '~/components/wrapper/page-wrapper'
 export default function FAQPage() {
   return (
     <PageWrapper>
-      <div className="w-full max-w-xl space-y-4">
-        <h1 className="font-bold text-4xl">Preguntas frecuentes</h1>
+      <div className="my-[8rem] flex w-full items-center justify-center">
+        <div className="flex w-[70%] flex-col lg:w-[50%]">
+          <h2 className="scroll-m-20 pb-[3rem] text-center font-semibold text-3xl tracking-tight lg:text-4xl">
+            Preguntas frecuentes (FAQs)
+          </h2>
 
-        <FAQSection />
+          <FAQSection />
+        </div>
       </div>
     </PageWrapper>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { FAQSection } from '~/components/home/faq'
 import { HeroSection } from '~/components/home/hero-section'
 import { SideBySide } from '~/components/home/side-by-side'
 import { PageWrapper } from '~/components/wrapper/page-wrapper'
@@ -11,6 +12,16 @@ export default function Home() {
 
       <div className="my-[8rem] flex w-full items-center justify-center">
         <SideBySide />
+      </div>
+
+      <div className="my-[8rem] flex w-full items-center justify-center">
+        <div className="flex w-[70%] flex-col lg:w-[50%]">
+          <h2 className="scroll-m-20 pb-[3rem] text-center font-semibold text-3xl tracking-tight lg:text-4xl">
+            Preguntas frecuentes (FAQs)
+          </h2>
+
+          <FAQSection />
+        </div>
       </div>
     </PageWrapper>
   )
