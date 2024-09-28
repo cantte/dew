091a6f73578c8ea419a371ba1d6e28d6abd87640
CarlosPavajeau <cantte098@gmail.com>
2024-09-28 11:05:44 -0500

    refactor(dashboard): update sidebar fallback

--- src/app/(dashboard)/dashboard/sidebar.server.tsx
+++ src/app/(dashboard)/dashboard/sidebar.server.tsx
@@ -15,9 +15,18 @@ export const DashboardServerSidebar = async () => {
 export const DashboardServerSidebarFallback = () => {
   return (
     <div className="grid gap-1">
-      <Skeleton className="h-8 w-full" />
-      <Skeleton className="h-8 w-full" />
-      <Skeleton className="h-8 w-full" />
+      <nav className="relative z-10 hidden h-screen w-[72px] flex-none border-r pt-20 duration-500 md:block">
+        <div className="space-y-4 py-4">
+          <div className="mt-3 px-3 py-2">
+            <nav className="grid items-start gap-2">
+              <Skeleton className="h-8 w-full" />
+              <Skeleton className="h-8 w-full" />
+              <Skeleton className="h-8 w-full" />
+              <Skeleton className="h-8 w-full" />
+            </nav>
+          </div>
+        </div>
+      </nav>
     </div>
   )
 }
