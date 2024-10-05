24ae431b6ca10c1cf8bcd26564090914a19fabfa
CarlosPavajeau <cantte098@gmail.com>
2024-10-05 15:02:22 -0500

    refactor(auth): update GoogleSignInButton component to use handleClick function

--- src/components/auth/google-signin.tsx
+++ src/components/auth/google-signin.tsx
@@ -9,15 +9,14 @@ export const GoogleSignInButton = () => {
   const searchParams = useSearchParams()
   const callbackUrl = searchParams.get('callbackUrl')
 
+  const handleClick = async () => {
+    await signIn('google', {
+      callbackUrl: callbackUrl ?? '/dashboard',
+    })
+  }
+
   return (
-    <Button
-      variant="outline"
-      onClick={() =>
-        void signIn('google', {
-          callbackUrl: callbackUrl ?? '/dashboard',
-        })
-      }
-    >
+    <Button variant="outline" onClick={handleClick}>
       <GoogleIcon className="mr-2 h-5 w-5" />
       <span className="whitespace-nowrap">Iniciar sesión con Google</span>
     </Button>
