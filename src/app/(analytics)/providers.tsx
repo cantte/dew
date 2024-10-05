0630b355bd4daedeac373e2ae050d42c7acf14e1
CarlosPavajeau <cantte098@gmail.com>
2024-10-05 14:33:11 -0500

    refactor(analytics): throw error if NEXT_PUBLIC_POSTHOG_KEY is not set

--- src/app/(analytics)/providers.tsx
+++ src/app/(analytics)/providers.tsx
@@ -4,7 +4,11 @@ import posthog from 'posthog-js'
 import { PostHogProvider } from 'posthog-js/react'
 
 if (typeof window !== 'undefined') {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  if (!process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+    throw new Error('NEXT_PUBLIC_POSTHOG_KEY is not set')
+  }
+
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
   })
@@ -14,6 +18,6 @@ type Props = {
   children: React.ReactNode
 }
 
-export function CSPostHogProvider({ children }: Props) {
+export function CSPostHogProvider({ children }: Readonly<Props>) {
   return <PostHogProvider client={posthog}>{children}</PostHogProvider>
 }
