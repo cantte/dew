8f21fbf131304ee4641645f7261bdb2ce5314081
CarlosPavajeau <cantte098@gmail.com>
2024-10-15 15:39:45 -0500

    feat(invite): finish invitation link page

--- src/app/(dashboard)/invite/[token]/page.tsx
+++ src/app/(dashboard)/invite/[token]/page.tsx
@@ -1,7 +1,11 @@
+import { FileWarning, LayoutDashboard } from 'lucide-react'
+import NextLink from 'next/link'
 import { redirect } from 'next/navigation'
-import DefaultLayout from '~/components/default-layout'
+import { AcceptInvitationButton } from '~/components/invite/accept-invitation-button'
+import { Alert, AlertDescription, AlertTitle } from '~/components/ui/alert'
 import { Button } from '~/components/ui/button'
 import { getServerAuthSession } from '~/server/auth'
+import { api } from '~/trpc/server'
 
 type Props = {
   params: {
@@ -17,23 +21,83 @@ export default async function StoreInvitationPage({ params }: Props) {
     )
   }
 
-  return (
-    <DefaultLayout>
-      <div className="flex min-h-[calc(100vh-10rem)] w-full flex-col items-center gap-4">
-        <div className="mx-auto my-auto flex flex-col gap-4 rounded border p-4">
-          <div className="flex flex-col gap-2">
-            <h1 className="scroll-m-20 font-semibold text-xl tracking-tight">
-              Hola, has sido invitado a la tienda.
-            </h1>
-            <p>
-              Has sido invitado a la tienda en Dew. Para acceder a la tienda,
-              haz clic en el siguiente botón:
-            </p>
-          </div>
+  const invitationLink = await api.employee.findInvitationLink({
+    token: params.token,
+  })
 
-          <Button>Aceptar invitación</Button>
+  if (!invitationLink) {
+    return (
+      <div className="flex min-h-screen w-full items-center justify-center p-4">
+        <Alert className="max-w-sm">
+          <AlertTitle className="flex items-center gap-1.5">
+            <FileWarning className="size-4 text-muted-foreground" />
+            <span>Error</span>
+          </AlertTitle>
+          <AlertDescription>
+            La invitación ha expirado o ya ha sido utilizada.
+            <br />
+            <Button asChild size="sm" className="mt-4">
+              <NextLink href={`/dashboard`}>
+                <LayoutDashboard className="mr-2 h-3.5 w-3.5" />
+                <span className="whitespace-nowrap">
+                  Volver al panel de control
+                </span>
+              </NextLink>
+            </Button>
+          </AlertDescription>
+        </Alert>
+      </div>
+    )
+  }
+
+  if (invitationLink.employee.email !== session.user.email) {
+    return (
+      <div className="flex min-h-screen w-full items-center justify-center p-4">
+        <Alert className="max-w-sm">
+          <AlertTitle className="flex items-center gap-1.5">
+            <FileWarning className="size-4 text-muted-foreground" />
+            <span>Error</span>
+          </AlertTitle>
+          <AlertDescription>
+            La invitación no corresponde a tu cuenta. El correo electrónico
+            registrado en la invitación es{' '}
+            <span className="font-medium">{invitationLink.employee.email}</span>
+            .
+            <br />
+            <Button asChild size="sm" className="mt-4">
+              <NextLink href={`/dashboard`}>
+                <LayoutDashboard className="mr-2 h-3.5 w-3.5" />
+                <span className="whitespace-nowrap">
+                  Volver al panel de control
+                </span>
+              </NextLink>
+            </Button>
+          </AlertDescription>
+        </Alert>
+      </div>
+    )
+  }
+
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center p-4">
+      <div className="w-full max-w-sm overflow-hidden rounded border">
+        <div className="space-y-6 p-8">
+          <h1 className="text-center font-light text-3xl text-foreground">
+            {invitationLink.store.name}
+          </h1>
+          <div className="mx-auto h-1 w-16 bg-gray-200"></div>
+          <p className="text-center text-muted-foreground text-sm">
+            Estimado{' '}
+            <span className="font-medium">{invitationLink.employee.name}</span>,
+          </p>
+          <p className="text-center text-foreground text-sm">
+            Has sido invitado a unirte a la tienda{' '}
+            <span className="font-medium">{invitationLink.store.name}</span>.
+          </p>
+
+          <AcceptInvitationButton token={params.token} />
         </div>
       </div>
-    </DefaultLayout>
+    </div>
   )
 }
