caef60f3896ff9610dc53d9fe5f83ad5037fdc89
CarlosPavajeau <CarlosPavajeau@users.noreply.github.com>
2024-10-20 17:07:04 +0000

    chore: format code

--- src/components/default-loading.tsx
+++ src/components/default-loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '~/components/ui/skeleton'
 
 const DefaultLoading = () => {
   return (
-    <div className='flex w-full flex-col space-y-3'>
+    <div className="flex w-full flex-col space-y-3">
       <Skeleton className="h-8 w-1/2" />
       <Skeleton className="h-4 w-1/3" />
     </div>
