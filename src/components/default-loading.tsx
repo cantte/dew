385413f7426d00790e78d12c5ea2aea536b5dda7
CarlosPavajeau <cantte098@gmail.com>
2024-10-20 11:53:18 -0500

    refactor: update loading component styles

--- src/components/default-loading.tsx
+++ src/components/default-loading.tsx
@@ -2,8 +2,8 @@ import { Skeleton } from '~/components/ui/skeleton'
 
 const DefaultLoading = () => {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-4 w-1/2" />
+    <div className='flex w-full flex-col space-y-3'>
+      <Skeleton className="h-8 w-1/2" />
       <Skeleton className="h-4 w-1/3" />
     </div>
   )
