c49c2ee9f54709eda4ba880d2197e6f0a822a2de
CarlosPavajeau <cantte098@gmail.com>
2024-11-19 11:24:36 -0500

    feat(sales): enhance SalesOverview component with OverviewCard and tooltips

--- src/components/sales/overview.tsx
+++ src/components/sales/overview.tsx
@@ -1,6 +1,12 @@
-import { CircleDollarSign, ShoppingCart, Tag } from 'lucide-react'
-import { Separator } from '~/components/ui/separator'
+import { CircleDollarSign, InfoIcon, ShoppingCart, Tag } from 'lucide-react'
+import { Badge } from '~/components/ui/badge'
 import { Skeleton } from '~/components/ui/skeleton'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '~/components/ui/tooltip'
 import { formatToCurrency, formatToNumber } from '~/text/format'
 
 type Props = {
@@ -12,67 +18,98 @@ type Props = {
 
 export const SalesOverview = ({ sales, products, amount, profit }: Props) => {
   return (
-    <div className="grid w-full grid-cols-1 gap-1 rounded-md border bg-card md:grid-cols-2 lg:grid-cols-4">
-      <div className="flex justify-between p-2">
-        <div className="p-2">
-          <div className="flex items-center space-x-2">
-            <ShoppingCart className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Ventas totales
-            </p>
-          </div>
-          <p className="font-semibold text-lg">
-            {formatToNumber('es-CO', sales)}
-          </p>
+    <TooltipProvider>
+      <div className="grid w-full grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-4">
+        <div>
+          <OverviewCard
+            icon={<ShoppingCart className="size-3.5 text-muted-foreground" />}
+            title="Ventas totales"
+            value={formatToNumber('es-CO', sales)}
+            variationRate={10}
+          />
         </div>
 
-        <Separator orientation="vertical" className="hidden md:flex" />
+        <div>
+          <OverviewCard
+            icon={<Tag className="size-3.5 text-muted-foreground" />}
+            title="Productos vendidos"
+            value={formatToNumber('es-CO', products)}
+            variationRate={-10}
+          />
+        </div>
+
+        <div>
+          <OverviewCard
+            icon={
+              <CircleDollarSign className="size-3.5 text-muted-foreground" />
+            }
+            title="Ingresos generados"
+            value={formatToCurrency('es-CO', amount)}
+          />
+        </div>
+
+        <div>
+          <OverviewCard
+            icon={
+              <CircleDollarSign className="size-3.5 text-muted-foreground" />
+            }
+            title="Ganancias generadas"
+            value={formatToCurrency('es-CO', profit)}
+          />
+        </div>
+      </div>
+    </TooltipProvider>
+  )
+}
+
+type OverviewCardProps = {
+  icon: React.ReactNode
+  title: string
+  value: string
+  variationRate?: number
+}
+
+const OverviewCard = ({
+  icon,
+  title,
+  value,
+  variationRate,
+}: OverviewCardProps) => {
+  return (
+    <div className="flex flex-col space-y-2 rounded-md border bg-card p-4">
+      <div className="flex items-center space-x-2">
+        {icon}
+        <p className="font-medium text-muted-foreground text-sm">{title}</p>
       </div>
 
-      <div className="flex justify-between p-2">
-        <div className="p-2">
+      <div className="flex items-center justify-between">
+        <p className="font-semibold text-xl leading-none tracking-tight md:text-2xl">
+          {value}
+        </p>
+
+        {variationRate && (
           <div className="flex items-center space-x-2">
-            <Tag className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Productos vendidos
-            </p>
+            <Badge variant={variationRate > 0 ? 'success' : 'destructive'}>
+              {variationRate > 0 ? '+' : ''}
+              {variationRate}%
+            </Badge>
+
+            <Tooltip>
+              <TooltipTrigger>
+                <InfoIcon className="size-3.5 text-muted-foreground" />
+              </TooltipTrigger>
+
+              <TooltipContent>
+                <div className="grid gap-1">
+                  <span>
+                    Representa la variación con respecto al periodo anterior.
+                  </span>
+                  <span>[ESTA FUNCIONALIDAD SE ENCUENTRA EN DESARROLLO]</span>
+                </div>
+              </TooltipContent>
+            </Tooltip>
           </div>
-          <p className="font-semibold text-lg">
-            {formatToNumber('es-CO', products)}
-          </p>
-        </div>
-
-        <Separator orientation="vertical" className="hidden lg:flex" />
-      </div>
-
-      <div className="flex justify-between p-2">
-        <div className="p-2">
-          <div className="flex items-center space-x-2">
-            <CircleDollarSign className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Ingesos generados
-            </p>
-          </div>
-          <p className="font-semibold text-lg">
-            {formatToCurrency('es-CO', amount)}
-          </p>
-        </div>
-
-        <Separator orientation="vertical" className="hidden md:flex" />
-      </div>
-
-      <div className="flex justify-between p-2">
-        <div className="p-2">
-          <div className="flex items-center space-x-2">
-            <CircleDollarSign className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Ganancias generadas
-            </p>
-          </div>
-          <p className="font-semibold text-lg">
-            {formatToCurrency('es-CO', profit)}
-          </p>
-        </div>
+        )}
       </div>
     </div>
   )
@@ -80,67 +117,21 @@ export const SalesOverview = ({ sales, products, amount, profit }: Props) => {
 
 export const SalesOverviewFallback = () => {
   return (
-    <div className="grid w-full grid-cols-1 gap-1 rounded border md:grid-cols-2 lg:grid-cols-4">
-      <div className="flex justify-between p-2">
-        <div className="p-2">
-          <div className="flex items-center space-x-2">
-            <ShoppingCart className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Ventas totales
-            </p>
-          </div>
-          <p className="font-semibold text-lg">
-            <Skeleton className="h-6 w-16" />
-          </p>
-        </div>
-
-        <Separator orientation="vertical" className="hidden md:flex" />
+    <div className="grid w-full grid-cols-1 gap-1 md:grid-cols-2 lg:grid-cols-4">
+      <div>
+        <Skeleton className="h-20" />
       </div>
 
-      <div className="flex justify-between p-2">
-        <div className="p-2">
-          <div className="flex items-center space-x-2">
-            <Tag className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Productos vendidos
-            </p>
-          </div>
-          <p className="font-semibold text-lg">
-            <Skeleton className="h-6 w-16" />
-          </p>
-        </div>
-
-        <Separator orientation="vertical" className="hidden lg:flex" />
+      <div>
+        <Skeleton className="h-20" />
       </div>
 
-      <div className="flex justify-between p-2">
-        <div className="p-2">
-          <div className="flex items-center space-x-2">
-            <CircleDollarSign className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Ingesos generados
-            </p>
-          </div>
-          <p className="font-semibold text-lg">
-            <Skeleton className="h-6 w-16" />
-          </p>
-        </div>
-
-        <Separator orientation="vertical" className="hidden md:flex" />
+      <div>
+        <Skeleton className="h-20" />
       </div>
 
-      <div className="flex justify-between p-2">
-        <div className="p-2">
-          <div className="flex items-center space-x-2">
-            <CircleDollarSign className="size-3.5 text-muted-foreground" />
-            <p className="font-medium text-muted-foreground text-sm">
-              Ganancias generadas
-            </p>
-          </div>
-          <p className="font-semibold text-lg">
-            <Skeleton className="h-6 w-16" />
-          </p>
-        </div>
+      <div>
+        <Skeleton className="h-20" />
       </div>
     </div>
   )
