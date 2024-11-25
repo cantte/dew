a7d4b9c6d801ce7e0714f8f87452f789a539ad2d
CarlosPavajeau <cantte098@gmail.com>
2024-11-25 15:00:46 -0500

    feat(stores): enhance deleteStore logic to update user preferences and handle non-existent stores

--- src/server/api/routers/stores/delete.ts
+++ src/server/api/routers/stores/delete.ts
@@ -1,8 +1,8 @@
-import { eq, sql } from 'drizzle-orm'
+import { and, desc, eq, isNull, sql } from 'drizzle-orm'
 import type { TypeOf } from 'zod'
 import type { TRPCAuthedContext } from '~/server/api/procedures/authed'
 import type { findStoreInput } from '~/server/api/schemas/stores'
-import { stores } from '~/server/db/schema'
+import { stores, userPreferences } from '~/server/db/schema'
 
 type Options = {
   ctx: TRPCAuthedContext
@@ -10,10 +10,34 @@ type Options = {
 }
 
 export const deleteStore = async ({ ctx, input }: Options) => {
-  return await ctx.db
+  const deleteStoreResult = await ctx.db
     .update(stores)
     .set({
       deletedAt: sql`CURRENT_TIMESTAMP`,
     })
     .where(eq(stores.id, input.id))
+
+  if (deleteStoreResult.count === 0) {
+    throw new Error(`[ERROR]: Cannot delete store with id ${input.id}`)
+  }
+
+  const [lastStore] = await ctx.db
+    .select({
+      id: stores.id,
+    })
+    .from(stores)
+    .where(
+      and(eq(stores.createdBy, ctx.session.user.id), isNull(stores.deletedAt)),
+    )
+    .orderBy(desc(stores.createdAt))
+    .limit(1)
+
+  await ctx.db
+    .update(userPreferences)
+    .set({
+      storeId: lastStore?.id ?? sql`NULL`,
+    })
+    .where(eq(userPreferences.userId, ctx.session.user.id))
+
+  return true
 }
