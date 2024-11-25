706d34f9790019b07967794a770ac3d4701dc0ee
CarlosPavajeau <cantte098@gmail.com>
2024-11-25 15:16:45 -0500

    feat(stores): implement confirmation dialog for store deletion

--- src/components/stores/delete-store.tsx
+++ src/components/stores/delete-store.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/navigation'
+import { Fragment, useEffect, useState } from 'react'
 import { Button } from '~/components/ui/button'
 import {
   Card,
@@ -6,35 +8,68 @@ import {
   CardHeader,
   CardTitle,
 } from '~/components/ui/card'
+import { api } from '~/trpc/react'
 import type { RouterOutputs } from '~/trpc/shared'
+import ConfirmDialog from '../confirm-dialog'
 
 type Props = {
   store: NonNullable<RouterOutputs['store']['findCurrent']>
 }
 
 export const DeleteStore = ({ store }: Props) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const deleteStore = api.store.delete.useMutation()
+
+  const handleDelete = () => {
+    deleteStore.mutate({ id: store.id })
+  }
+
+  const utils = api.useUtils()
+  const router = useRouter()
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: not needed
+  useEffect(() => {
+    if (deleteStore.isSuccess) {
+      utils.store.findCurrent.invalidate()
+      utils.store.list.invalidate()
+      router.refresh()
+      router.push('/dashboard')
+    }
+  }, [deleteStore.isSuccess])
+
   return (
-    <Card className="border-destructive">
-      <CardHeader>
-        <div className="flex items-center gap-2">
-          <CardTitle className="text-destructive text-xl">
-            Eliminar tienda
-          </CardTitle>
-        </div>
-      </CardHeader>
+    <Fragment>
+      <ConfirmDialog
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        onConfirm={handleDelete}
+        title="Eliminar tienda"
+        description="¿Estás seguro que deseas eliminar esta tienda?"
+      />
 
-      <CardContent>
-        <p className="text-destructive/80">
-          Si deseas eliminar tu tienda, puedes hacerlo aquí. Ten en cuenta que
-          esta acción no se puede deshacer.
-        </p>
-      </CardContent>
+      <Card className="border-destructive">
+        <CardHeader>
+          <div className="flex items-center gap-2">
+            <CardTitle className="text-destructive text-xl">
+              Eliminar tienda
+            </CardTitle>
+          </div>
+        </CardHeader>
 
-      <CardFooter>
-        <Button variant="destructive" disabled>
-          Deseo eliminar mi tienda
-        </Button>
-      </CardFooter>
-    </Card>
+        <CardContent>
+          <p className="text-destructive/80">
+            Si deseas eliminar tu tienda, puedes hacerlo aquí. Ten en cuenta que
+            esta acción no se puede deshacer.
+          </p>
+        </CardContent>
+
+        <CardFooter>
+          <Button variant="destructive" onClick={() => setIsOpen(true)}>
+            Deseo eliminar mi tienda
+          </Button>
+        </CardFooter>
+      </Card>
+    </Fragment>
   )
 }
